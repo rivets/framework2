6625fe94229396391b08a5b8464389eb17dedf7f
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2017-10-03 11:47:16 +0100

    Add a base class for ajax operations.

--- class/ajax.php
+++ class/ajax.php
@@ -12,7 +12,20 @@
     class Ajax extends \Framework\Ajax
     {
 /**
- * Any Ajax for your system go in here.
+ * Any Ajax for your system goes in here.
+ *
+ *********  Make sure that you call the parent handle method for anything you are not handling yourself!! ***********
  */
+/**
+ * Handle AJAX operations
+ *
+ * @param object	$context	The context object for the site
+ *
+ * @return void
+ */
+        public function handle($context)
+        {
+            parent::handle($context);
+        }
     }
 ?>
