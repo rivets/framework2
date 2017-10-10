271bc82b84b9fb0a3062de6e88d2c4f85d189fc2
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2017-10-10 19:43:27 +0100

    Test for php and integerate getcomposer into firstrun

--- firstrun.sh
+++ firstrun.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-if [ ! -d vendor/twig ]
-then # looks like composer has not been run
-    composer install
+if which php >/dev/null
+then # we do have PHP
+    if [ ! -d vendor/twig ]
+    then # looks like composer has not been run
+        if which composer >/dev/null
+        then # we have composer
+            composer install
+        else
+            curl -sS https://getcomposer.org/installer | php
+            php composer.phar install
+        fi
+    fi
+    for i in . .htaccess class/config assets
+    do
+        chmod a+w $i
+    done
+else
+    echo '******* Cannot find PHP!'
 fi
-for i in . .htaccess class/config assets
-do
-    chmod a+w $i
-done
+
