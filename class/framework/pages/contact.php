5c7b81a81238c089040180c6a0b090e33813bec3
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2017-10-17 15:24:39 +0100

    Use new mailer class

--- class/framework/pages/contact.php
+++ class/framework/pages/contact.php
@@ -25,7 +25,16 @@
             $fd = $context->formdata();
             if (($msg = $fd->post('message', '')) !== '')
             { # there is a post
-                mail(Config::SYSADMIN, $fd->post('subject', 'No Subject'), $fd->post('sender', 'No Sender').PHP_EOL.PHP_EOL.$msg);
+                $mail = new \Utility\FMailer;
+                $mail->setFrom(Config::SITENOREPLY);
+                $mail->addReplyTo(Config::SITENOREPLY);
+                $mail->addAddress(Config::SYSADMIN);
+
+                $mail->Subject = $fd->post('subject', 'No Subject');
+                $mail->AltBody= $msg;
+                $mail->send();
+
+//                mail(Config::SYSADMIN, $fd->post('subject', 'No Subject'), $fd->post('sender', 'No Sender').PHP_EOL.PHP_EOL.$msg);
                 $context->local()->message(Local::MESSAGE, 'Thank you. We will be in touch as soon as possible.');
             }
             return 'contact.twig';
