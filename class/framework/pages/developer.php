58fa22c96fe0ba99aecd09096b4f5082056b4219
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2017-10-17 15:21:08 +0100

    Formatting

--- class/framework/pages/developer.php
+++ class/framework/pages/developer.php
@@ -24,37 +24,37 @@
 	{
 	    if ($context->hasdeveloper())
 	    {
-		$tpl = 'support/devel.twig';
-                $rest = $context->rest();
-		switch ($rest[0])
-		{
-                case 'hack': # execute some code.
-                    \R::freeze(FALSE); // turn off freezing so that you can fiddle with the database....
-                    include $context->local()->makebasepath('devel', 'hack.php');
-                    break;
-
-                case 'fail': # this lets you test error handling
-                    $x = 2 / 0;
-                    break;
-
-                case 'throw': # this lets you test exception handling
-                    throw new \Exception('Unhandled Exception Test');
-
-		case 'mail' : # this lets you test email sending
-		    $foo = mail($context->user()->email, 'test', 'test');
-		    $context->local()->message(\Framework\Local::MESSAGE, 'sent');
-		    break;
+            $tpl = 'support/devel.twig';
+            $rest = $context->rest();
+            switch ($rest[0])
+            {
+                    case 'hack': # execute some code.
+                        \R::freeze(FALSE); // turn off freezing so that you can fiddle with the database....
+                        include $context->local()->makebasepath('devel', 'hack.php');
+                        break;
+    
+                    case 'fail': # this lets you test error handling
+                        $x = 2 / 0;
+                        break;
+    
+                    case 'throw': # this lets you test exception handling
+                        throw new \Exception('Unhandled Exception Test');
+    
+            case 'mail' : # this lets you test email sending
+                $foo = mail($context->user()->email, 'test', 'test');
+                $context->local()->message(\Framework\Local::MESSAGE, 'sent');
+                break;
 /*
-		case 'errlog' : # this will show you the contents of the PHP error log file.
-		    $context->local()->addval('errlog', file_get_contents(Config::PHPLOG));
-		    exit;
-
-		case 'clearlog' :
-		    fclose(fopen(Config::PHPLOG, 'w'));
-		    $context->local()->message(Local::MESSAGE, 'Log Cleared');
-		    break;
+            case 'errlog' : # this will show you the contents of the PHP error log file.
+                $context->local()->addval('errlog', file_get_contents(Config::PHPLOG));
+                exit;
+    
+            case 'clearlog' :
+                fclose(fopen(Config::PHPLOG, 'w'));
+                $context->local()->message(Local::MESSAGE, 'Log Cleared');
+                break;
 */
-                }
+            }
 	    }
 	    else
 	    {
