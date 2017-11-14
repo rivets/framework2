af9cbcdba30377495b830b762224a61019bc8334
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2017-11-14 16:15:24 +0000

    Fix typo this - > fd

--- class/framework/pages/upload.php
+++ class/framework/pages/upload.php
@@ -29,7 +29,7 @@
             {
                 if (Config::UPUBLIC && Config::UPRIVATE)
                 { # need to check the flag could be either private or public
-                    foreach ($this->posta('public') as $ix => $public)
+                    foreach ($fd->posta('public') as $ix => $public)
                     {
                         $upl = \R::dispense('upload');
                         $upl->savefile($context, $fd->filedata('uploads', $ix), $public, $context->user());
