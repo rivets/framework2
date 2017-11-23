b06435e269981b5d5a758235bd5b2e148e48de72
Ben Gerard <bengerard@hotmail.co.uk>
2017-11-23 13:58:30 +0000

    Fixed vagrant setup.

--- vagrant/provision_vagrant.sh
+++ vagrant/provision_vagrant.sh
@@ -1,13 +1,10 @@
 # Sets up Vagrant box for use with The Framework
-sudo apt-get install -y language-pack-en-base
-sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-sudo apt-get install -y apache2 mysql-server php7.0-mysql libapache2-mod-php git-core unzip php7.0-mbstring
+#!/bin/bash
+sudo git clone https://github.com/rivets/framework2.git /var/www/public/webproject
+cd /var/www/public/webproject
+sudo composer install
 
-sudo rm -rf /var/www/html
-sudo ln -s /vagrant /var/www/html
-sudo cp /vagrant/vagrant/99-phpsettings.ini /etc/php/7.0/apache2/conf.d/
-sudo service apache2 restart
 
 echo "Vagrant box has been setup for The Framework"
-echo "Please complete setup from http://framework.dev/install.php"
+echo "Please complete setup from http://192.168.33.10/webproject/install.php"
+echo "The mysql root password is root"
