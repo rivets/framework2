04b1e713ccc8c6634609fea4250415e2fb532f71
rivets <lindsay@marshall.name>
2017-12-08 16:13:14 +0000

    Add some semis

--- assets/js/bgfade.js
+++ assets/js/bgfade.js
@@ -1,13 +1,13 @@
     function easeInOut(minValue, maxValue, totalSteps, actualStep, powr)
     {
-	return Math.ceil(minValue + (Math.pow(((1 / totalSteps) * actualStep), powr) * (maxValue - minValue)))
+	return Math.ceil(minValue + (Math.pow(((1 / totalSteps) * actualStep), powr) * (maxValue - minValue)));
     }
 
     function doBGFade(elem, startRGB, endRGB, finalColor, steps, intervals, powr)
     {
 	if (elem.bgFadeInt)
         {
-            window.clearInterval(elem.bgFadeInt)
+            window.clearInterval(elem.bgFadeInt);
         }
 	var actStep = 0;
 	elem.bgFadeInt = window.setInterval(
@@ -16,14 +16,14 @@
 			    easeInOut(startRGB[0], endRGB[0], steps, actStep, powr) + ',' +
 			    easeInOut(startRGB[1], endRGB[1], steps, actStep, powr) + ',' +
 			    easeInOut(startRGB[2], endRGB[2], steps, actStep, powr) + ')'
-		    )
+		    );
 		    actStep += 1;
 		    if (actStep > steps)
                     {
-		        elem.css('backgroundcolor', finalColor)
-		        window.clearInterval(elem.bgFadeInt)
+		        elem.css('backgroundcolor', finalColor);
+		        window.clearInterval(elem.bgFadeInt);
 		    }
 	    },
 	    intervals
-        )
+        );
     }
