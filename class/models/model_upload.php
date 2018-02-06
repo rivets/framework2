11c7aa8a9fd6927973cea697b1afef420a9254ba
rivets <lindsay.marshall@ncl.ac.uk>
2018-02-06 11:06:33 +0000

    Remove method
    canaccess is defined in the trait so that users can easily update the
    access control.

--- class/models/model_upload.php
+++ class/models/model_upload.php
@@ -11,23 +11,7 @@
  */
     class Model_Upload extends \RedBeanPHP\SimpleModel
     {
-
         use \ModelExtend\Upload;
-
-/**
- * Determine if a user can access the file
- *
- * At the moment it is either the user or any admin that is allowd. Rewrite the
- * method to add more complex access control schemes.
- *
- * @param object	$user	A user object
- *
- * @return boolean
- */
-	public function canaccess($user)
-	{
-	    return $this->bean->user->equals($user) || $user->isadmin();
-	}
 /**
  * Return the owner of this uplaod
  *
