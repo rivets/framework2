a054111d0a2513e43f32dbc75aa7dcca7c37bf75
rivets <lindsay.marshall@ncl.ac.uk>
2018-02-06 22:31:38 +0000

    Format and Config updates
    Put in support for remote updating of essential URLS e.g. bootstrap.

--- class/framework/pages/admin.php
+++ class/framework/pages/admin.php
@@ -30,112 +30,135 @@
             switch ($rest[0])
             {
             case 'pages':
-            $tpl = '@admin/pages.twig';
-            break;
+                $tpl = '@admin/pages.twig';
+                break;
 
             case 'contexts':
-            $tpl = '@admin/contexts.twig';
-            break;
+                $tpl = '@admin/contexts.twig';
+                break;
 
             case 'roles':
-            $tpl = '@admin/roles.twig';
-            break;
+                $tpl = '@admin/roles.twig';
+                break;
 
             case 'users':
-            $tpl = '@admin/users.twig';
-            break;
+                $tpl = '@admin/users.twig';
+                break;
 
             case 'forms':
-            $tpl = '@admin/forms.twig';
-            break;
+                $tpl = '@admin/forms.twig';
+                break;
 
             case 'config':
-            $tpl = '@admin/config.twig';
-            break;
+                $tpl = '@admin/config.twig';
+                break;
 
             case 'info':
-            $_SERVER['PHP_AUTH_PW'] = '*************'; # hide the password in case it is showing.
+                $_SERVER['PHP_AUTH_PW'] = '*************'; # hide the password in case it is showing.
                 phpinfo();
-            exit;
+                exit;
 
             case 'edit' : // Edit something - forms, user, pages...
                 if (count($rest) < 3)
-                    {
+                {
+                    $context->web()->bad();
+                    /* NOT REACHED */
+                }
+                $kind = $rest[1];
+                if (!in_array($kind, self::EDITABLE))
+                {
+                    $context->web()->bad();
+                    /* not reached */
+                }
+                $obj = $context->load($kind, $rest[2]);
+                if (($bid = $context->formdata()->post('bean', '')) !== '')
+                { // this is a post
+                    if ($bid != $obj->getID())
+                    { # something odd...
                         $context->web()->bad();
                         /* NOT REACHED */
                     }
-                    $kind = $rest[1];
-                    if (!in_array($kind, self::EDITABLE))
-                    {
-                        $context->web()->bad();
-                        /* not reached */
-                    }
-                    $obj = $context->load($kind, $rest[2]);
-                    if (($bid = $context->formdata()->post('bean', '')) !== '')
-                    { // this is a post
-                        if ($bid != $obj->getID())
-                        { # something odd...
-                            $context->web()->bad();
-                            /* NOT REACHED */
-                        }
-                        $obj->edit($context);
-                        // The edit call might divert to somewhere else so sometimes we may not get here.
-                    }
-                    $context->local()->addval('bean', $obj);
-                    $tpl = '@edit/'.$kind.'.twig';
-                    break;
+                    $obj->edit($context);
+                    // The edit call might divert to somewhere else so sometimes we may not get here.
+                }
+                $context->local()->addval('bean', $obj);
+                $tpl = '@edit/'.$kind.'.twig';
+                break;
 
             case 'view' : // view something - forms only at the moment
                 if (count($rest) < 3)
-                    {
-                        $context->local()->bad();
-                    }
-                    $kind = $rest[1];
-                    if (!in_array($kind, self::VIEWABLE))
-                    {
-                        $context->web()->bad();
-                        /* NOT REACHED */
-                    }
-                    $obj = $context->load($kind, $rest[2]);
-                    $context->local()->addval($kind, $obj);
-                    $tpl = '@view/'.$kind.'.twig';
-                    break;
+                {
+                    $context->local()->bad();
+                }
+                $kind = $rest[1];
+                if (!in_array($kind, self::VIEWABLE))
+                {
+                    $context->web()->bad();
+                    /* NOT REACHED */
+                }
+                $obj = $context->load($kind, $rest[2]);
+                $context->local()->addval($kind, $obj);
+                $tpl = '@view/'.$kind.'.twig';
+                break;
 
             case 'update':
-                    $ufd = fopen('https://catless.ncl.ac.uk/frameworknew/update/', 'r');
-                    if ($ufd)
-                    {
-                        $upd = json_decode(fread($ufd, 1024));
-                        $context->local()->addval('version', $upd->version);
-                        fclose($ufd);
-                        $context->local()->addval('current', trim(file_get_contents($context->local()->makebasepath('version.txt'))));
-                    }
-                    elseif (function_exists('zip_open'))
-                    {
-                        $formd = $context->formdata();
-                        if ($formd->hasfile('update'))
+                $ufd = fopen('https://catless.ncl.ac.uk/frameworknew/update/', 'r');
+                if ($ufd)
+                {
+                    $upd = json_decode(fread($ufd, 1024));
+                    if (isset($upd->config))
+                    { // now see if there are any config values that need updating.
+                        foreach ($upd->config as $cname => $cvalue)
                         {
-                            $data = $formd->filedata('update');
-                            if (($zd = zip_open($data['tmp_name'])) === FALSE)
+                            $lval = \R::findOne('fwconfig', 'name=?', [$cname]);
+                            if (is_object($lval))
                             {
-                                $context->local()->message(Local::ERROR, 'Cannot open the file');
+                                if ($lval->local == 0 && $lval->value != $cvalue)
+                                { // update if not locally set and there is a new value
+                                    $lval->value = $cvalue;
+                                    \R::store($lval);
+                                }
                             }
                             else
                             {
-                                $context->local()->message(Local::MESSAGE, 'Done');
+                                $lval = \R::dispense('fwconfig');
+                                $lval->name = $cname;
+                                $lval->value = $cvalue;
+                                $lval->local = 0;
+                                \R::store($lval);
                             }
                         }
                     }
-                    else
+                    $context->local()->addval('version', $upd->version);
+                    fclose($ufd);
+                    $context->local()->addval('current', trim(file_get_contents($context->local()->makebasepath('version.txt'))));
+                }
+                elseif (function_exists('zip_open'))
+                {
+                    $formd = $context->formdata();
+                    if ($formd->hasfile('update'))
                     {
-                        $context->local()->addval('nozip', TRUE);
+                        $data = $formd->filedata('update');
+                        if (($zd = zip_open($data['tmp_name'])) === FALSE)
+                        {
+                            $context->local()->message(Local::ERROR, 'Cannot open the file');
+                        }
+                        else
+                        {
+                            $context->local()->message(Local::MESSAGE, 'Done');
+                        }
                     }
-                    $tpl = '@admin/update.twig';
-                    break;
+                }
+                else
+                {
+                    $context->local()->addval('nozip', TRUE);
+                }
+                $tpl = '@admin/update.twig';
+                break;
 
             default :
-                    $tpl = '@admin/admin.twig';
-                    break;
+                $tpl = '@admin/admin.twig';
+                break;
             }
             return $tpl;
         }
