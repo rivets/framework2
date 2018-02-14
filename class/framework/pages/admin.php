817ef003da9e1545c22cf8c7726c3315a1708ca2
Lindsay Marshall <lindsay.marshall@newcastle.ac.uk>
2018-02-14 10:04:03 +0000

    Ad config updating to the update code.

--- class/framework/pages/admin.php
+++ class/framework/pages/admin.php
@@ -106,6 +106,33 @@
                 if ($ufd)
                 {
                     $upd = json_decode(fread($ufd, 1024));
+                    if (isset($upd->config))
+                    { // now see if there are any config values that need updating.
+                        foreach ($upd->config as $cname => list($cvalue, $cfixed))
+                        {
+                            $lval = \R::findOne('fwconfig', 'name=?', [$cname]);
+                            if (is_object($lval))
+                            {
+                                if (/*$lval->local == 0 && */$lval->value != $cvalue)
+                                { // update if not locally set and there is a new value
+                                    $lval->value = $cvalue;
+                                    $lval->fixed = $cfixed;
+                                    $lval->local = 0; // Once everyone has the local column this can go away
+                                    $lval->fixed = $cfixed;
+                                    \R::store($lval);
+                                }
+                            }
+                            else
+                            {
+                                $lval = \R::dispense('fwconfig');
+                                $lval->name = $cname;
+                                $lval->value = $cvalue;
+                                $lval->local = 0;
+                                $lval->fixed = $cfixed;
+                                \R::store($lval);
+                            }
+                        }
+                    }
                     $context->local()->addval('version', $upd->version);
                     fclose($ufd);
                     $context->local()->addval('current', trim(file_get_contents($context->local()->makebasepath('version.txt'))));
