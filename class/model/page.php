771c46f3d31feafd96a0bb9983a3df8c61d1b3de
rivets <lindsay@marshall.name>
2018-07-12 11:02:01 +0100

    Regexp fixes

--- class/model/page.php
+++ class/model/page.php
@@ -71,13 +71,20 @@
         {
             if (preg_match('%@content/(.*)%'. $name, $m))
             {
-                $name = $m[1];
+                $name = 'content/'.$m[1];
             }
             elseif (preg_match('%@([a-z]+)/(.*)%'. $name, $m))
             {
                 $name = 'framework/'.$m[1]/$m[2];
             }
-            $name = preg_replace('/(\.twig)*$/', '.twig', $name); // this adds a missing .twig and removes extra .twigs .....
+            if (!preg_match('/\.twig$', $name))
+            {
+                $name .= '.twig';
+            }
+            else
+            {
+                $name = preg_replace('/(\.twig)+$/', '.twig', $name); // this removes extra .twigs .....
+            }
             $file = $context->local()->makebasepath('twigs', $name);
             if (!file_exists($file))
             { // make the file
