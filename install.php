57d51a3387f101d9e9f9a735759e923f2283d84d
rivets <lindsay@marshall.name>
2018-08-15 11:34:19 +0100

    Add flag
    missed out async flag value...

--- install.php
+++ install.php
@@ -287,19 +287,19 @@
  */
     $fwurls = [
 // CSS
-        'bootcss'       => ['//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', 1, '', '', 0, 'css'],
-//        'editablecss'   => ['//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/css/bootstrap-editable.css', 1, '', '', 0, 'css'],
-        'editablecss'   => ['/'.$dir.'/assets/css/bs4-editable.css', 1, '', '', 0, 'css'],
-        'facss'         => ['//use.fontawesome.com/releases/v5.2.0/css/all.css', 1, '', '', 0, 'css'],
+        'bootcss'       => ['//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', 1, '', '', 0, 0, 'css'],
+//        'editablecss'   => ['//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/css/bootstrap-editable.css', 1, '', '', 0, 0, 'css'],
+        'editablecss'   => ['/'.$dir.'/assets/css/bs4-editable.css', 1, '', '', 0, 0, 'css'],
+        'facss'         => ['//use.fontawesome.com/releases/v5.2.0/css/all.css', 1, '', '', 0, 0, 'css'],
 // JS
-        'jquery'       => ['https://code.jquery.com/jquery-3.3.1.min.js', 1, '', '', 0, 'js'],
-        'jqueryslim'     => ['https://code.jquery.com/jquery-3.3.1.slim.min.js', 1, '', '', 0, 'js'],
-        'bootjs'        => ['//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', 1, '', '', 0, 'js'],
-        'bootbox'       => ['//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js', 1, '', '', 0, 'js'],
-//        'editable'      => ['//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min.js', 1, '', '', 0, 'js'],
-        'editable'      => [($dir != '/' ? ('/'.$dir) : '' ).'/assets/js/bs4-editable-min.js', 1, '', '', 0, 'js'],
-        'parsley'       => ['//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.1/parsley.min.js', 1, '', '', 0, 'js'],
-        'popperjs'      => ['//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 1, '', '', 0, 'js'],
+        'jquery'       => ['https://code.jquery.com/jquery-3.3.1.min.js', 1, '', '', 0, 0, 'js'],
+        'jqueryslim'     => ['https://code.jquery.com/jquery-3.3.1.slim.min.js', 1, '', '', 0, 0, 'js'],
+        'bootjs'        => ['//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', 1, '', '', 0, 0, 'js'],
+        'bootbox'       => ['//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js', 1, '', '', 0, 0, 'js'],
+//        'editable'      => ['//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min.js', 1, '', '', 0, 0, 'js'],
+        'editable'      => [($dir != '/' ? ('/'.$dir) : '' ).'/assets/js/bs4-editable-min.js', 1, '', '', 0, 0, 'js'],
+        'parsley'       => ['//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.1/parsley.min.js', 1, '', '', 0, 0, 'js'],
+        'popperjs'      => ['//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 1, '', '', 0, 0, 'js'],
     ];
     
     $fwcsp = [
