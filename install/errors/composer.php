75b182f5d20ef5085c954cf825b67bf10f9b8c7a
rivets <lindsay@marshall.name>
2018-09-12 15:54:52 +0100

    Error if composer has not run

---
+++ install/errors/composer.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Error</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!--
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+-->
+
+</head>
+<body lang="en-GB">
+    <h1 style="margin-top: 5em; text-align: center">Please run composer to install the necessary support software.</h1>
+</body>
+</html>
