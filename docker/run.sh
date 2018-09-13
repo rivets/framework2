e863fad74310788e713a8b29949f2733ca72f1c2
rivets <lindsay@marshall.name>
2018-09-13 08:45:20 +0100

    Initialise the framework when initialising the DB

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,9 @@ if [ -e /var/lib/mysql/empty ]
 then
    rm /var/lib/mysql/empty
    /usr/sbin/mysqld --initialize-insecure --init-file=/tmp/init.sql
+   cd /var/www/html
+   ./firstrun.sh
+   cd /
 fi
 /etc/init.d/mysql start
 /usr/sbin/apache2ctl -DFOREGROUND -k start
