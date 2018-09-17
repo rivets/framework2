794e5e136a480ae83729421c534ab81ef6371f0f
rivets <lindsay@marshall.name>
2018-09-17 10:03:44 +0100

    Move the docker stuff out

--- docker/init.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE framework;
-GRANT ALL ON framework.* to framework@localhost identified by 'framework';
--- docker/rundocker.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -d -p 80:80 -v `pwd`:/var/www/html -v `pwd`/docker/db:/var/lib/mysql framework $*
