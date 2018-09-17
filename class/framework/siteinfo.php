39b4bb6097ae0043ee7c48742e4b289262b1bfad
rivets <lindsay@marshall.name>
2018-09-17 13:15:10 +0100

    Chnage t use integers only
    Rather than mixing with empty strings

--- class/framework/siteinfo.php
+++ class/framework/siteinfo.php
@@ -28,7 +28,7 @@
  */
         public function collect(string $bean, string $where, array $params, int $start, int $count)
         {
-            if ($start !== '')
+            if ($start >= 0)
             {
                  $where .= ' LIMIT '.$count.' OFFSET '.(($start - 1)*$count);
             }
@@ -51,7 +51,7 @@
  */
         public function fetch(string $bean, string $where, array $params, int $start, int $count) : array
         {
-            if ($start !== '')
+            if ($start >= 0)
             {
                  $where .= ' LIMIT '.$count.' OFFSET '.(($start - 1)*$count);
             }
@@ -77,84 +77,84 @@
 /**
  * Get all the user beans
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int     $start      Start position - used for pagination
+ * @param int     $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param bool      $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function users($start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function users(int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('user', $order !== '' ? $order : ' order by login', [], $start, $count);
         }
 /**
  * Get all the page beans
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param bool      $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function pages($start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function pages(int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('page', $order !== '' ? $order : ' order by name', [], $start, $count);
         }
 /**
  * Get all the Rolename beans
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param bool      $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function roles($start = '', $count = '', string $order = '', $collect = FALSE) : array
+        public function roles(int $start = -1, int $count = -1, string $order = '', $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('rolename', $order !== '' ? $order : ' order by name', [], $start, $count);
         }
 /**
  * Get all the Rolecontext beans
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param bool      $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function contexts($start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function contexts(int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('rolecontext', $order !== '' ? $order : ' order by name', [], $start, $count);
         }
 /**
  * Get all the site config information
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param boolean   $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function siteconfig($start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function siteconfig(int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('fwconfig', $order, [], $start, $count);
         }
 /**
  * Get all the form beans
  *
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param bool      $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function forms($start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function forms(int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             return $this->{$collect ? 'collect' : 'fetch'}('form', $order !== '' ? $order : ' order by name', [], $start, $count);
         }
@@ -163,14 +163,14 @@
  * @param mixed     $rolecontext
  * @param mixed     $rolename
  * @param boolean   $all            If TRUE do not check if role is currentyl active
- * @param mixed     $start      Start position - used for pagination
- * @param mixed     $count      The number to be fetched - used for pagination
+ * @param int       $start      Start position - used for pagination
+ * @param int       $count      The number to be fetched - used for pagination
  * @param string    $order      An order clause
  * @param boolean   $collect    If TRUE then use collect not fetch
  *
  * @return array
  */
-        public function usersWith($rolecontext, $rolename, bool $all = FALSE, $start = '', $count = '', string $order = '', bool $collect = FALSE) : array
+        public function usersWith($rolecontext, $rolename, bool $all = FALSE, int $start = -1, int $count = -1, string $order = '', bool $collect = FALSE) : array
         {
             $context = Context::getinstance();
             $rnid = is_object($rolename) ? $rolename->getID() : $context->rolename($rolename)->getID();
