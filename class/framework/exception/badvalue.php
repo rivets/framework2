8ea5eb65dcf60867d4c87a25591102e39868b035
rivets <lindsay@marshall.name>
2018-12-11 11:41:22 +0000

    New BadValue exception

---
+++ class/framework/exception/badvalue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Exception Class for when a bean update detects an error
+ *
+ * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
+ * @copyright 2018 Newcastle University
+ */
+    namespace Framework\Exception;
+/**
+ * Bean update error exception class
+ */
+    class BadValue extends \Exception
+    {
+    }
+?>
