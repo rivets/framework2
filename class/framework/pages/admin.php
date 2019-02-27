9347e2d88ca23a746888fac8d811a187f08bdb8c
rivets <lindsay@marshall.name>
2019-02-27 10:53:19 +0000

    Using constants for framework bean names

--- class/framework/pages/admin.php
+++ class/framework/pages/admin.php
@@ -17,7 +17,7 @@
  */
     class Admin extends \Framework\Siteaction
     {
-        const EDITABLE = ['table', 'form', 'fwconfig', 'page', 'user'];
+        const EDITABLE = ['table', 'form', Config::FWCONFIG, 'page', 'user'];
         const VIEWABLE = ['table', 'form'];
         const NOTMODEL = ['table'];
         const HASH     = 'sha384';
@@ -170,7 +170,7 @@
                 $base = $context->local()->base();
                 foreach ($upd->fwconfig as $cname => $cdata)
                 {
-                    $lval = \R::findOne('fwconfig', 'name=?', [$cname]);
+                    $lval = \R::findOne(Config::FWCONFIG, 'name=?', [$cname]);
                     if (is_object($lval))
                     {
                         if ($lval->local == 0)
@@ -194,7 +194,7 @@
                     }
                     else
                     {
-                        $lval = \R::dispense('fwconfig');
+                        $lval = \R::dispense(Config::FWCONFIG);
                         $lval->name = $cname;
                         $lval->local = 0;
                         foreach ($cdata as $k => $v)
