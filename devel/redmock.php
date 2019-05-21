728c7503c9e0a4f41801914f6539393b5df774f4
rivets <lindsay@marshall.name>
2019-05-21 13:58:56 +0100

    Add with

--- devel/redmock.php
+++ devel/redmock.php
@@ -2,12 +2,21 @@
     namespace RedBeanPHP;
     class OODBBean
     {
+        private static $dummy;
 /**
  * @return int
  */
         public function getID() : int
         {
              return 0;
+        }
+/**
+ * @param string $x
+ * @return \RedBeanPHP\OODBean
+ */
+        public function with(string $x) : \RedBeanPHP\OODBean
+        {
+             return self::$dummy;
         }
     }
 ?>
