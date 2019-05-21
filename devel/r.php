e3131c5d0157410c58629e9cf285fa6f580e6d6e
rivets <lindsay@marshall.name>
2019-05-21 13:17:28 +0100

    Try to mock up RedBean to shut up some of the psalm errors that arise.

---
+++ devel/r.php
@@ -0,0 +1,60 @@
+<?php
+
+// namespace RedBeanPHP;
+namespace \ ;
+
+/**
+ * R-Facade (for Composer)
+ *
+ * If you use Composer you don't use the rb.php file which
+ * has the R-facade, so here is a separate, namespaced R-facade for
+ * those that prefer this.
+ *
+ * An alternative option might be to alias RedBeanPHP/Facade.
+ *
+ * @file    RedBeanPHP/R.php
+ * @author  Simirimia
+ * @license BSD/GPLv2
+ *
+ */
+class R
+{
+    public static function dispense(string $b) : object
+    {
+        return new stdClass;
+    }
+
+    public static function store(object $b) : int
+    {
+        return 56;
+    }
+
+    public static function load(string $b, int $id) : object
+    {
+        return new stdClass;
+    }
+
+    public static function find(string $b, string $x = '', array $y = []) : array
+    {
+        return [];
+    }
+
+    public static function findAll(string $b, string $x = '', array $y = []) : array
+    {
+        return [];
+    }
+
+    public static function findOne(string $b, string $x = '', array $y = []) : object
+    {
+        return new stdClass;
+    }
+
+    public static function trash(object) : void
+    {
+    }
+
+    public static function inspect(string $x = '') : array
+    {
+        return [];
+    }
+}
