6b66a5f52604717593f42fb048001345cf5761ef
rivets <lindsay@marshall.name>
2019-06-07 15:01:18 +0100

    reformat psalm condition

--- class/framework/dispatch.php
+++ class/framework/dispatch.php
@@ -97,8 +97,10 @@
 /*
  * Look in the database for what to do based on the first part of the URL. DBRX means do a regep match
  */
-            /** @psalm-suppress TypeDoesNotContainType  */
-            /** @psalm-suppress RedundantCondition */
+            /**
+             * @psalm-suppress TypeDoesNotContainType
+             * @psalm-suppress RedundantCondition
+             */
             $page = \R::findOne(FW::PAGE, 'name'.(Config::DBRX ? ' regexp ' : '=').'? and active=?', [$action, 1]);
             if (!is_object($page))
             { # No such page or it is marked as inactive
