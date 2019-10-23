b706ad161837984fd80503713c99deb6b12a38af
rivets <lindsay@marshall.name>
2019-10-23 15:37:44 +0100

    Ad error checking to constructor

--- class/framework/faiterator.php
+++ class/framework/faiterator.php
@@ -19,9 +19,16 @@
  * The constructor
  *
  * @param string  $name The name of the entry in the $_FILES array
+ *
+ * throws \Exception
  */
         public function __construct(string $name)
         {
+            if (!isset($_FILES[$name]) || !is_array($_FILES[$name]['error']))
+            {
+                throw new \Exception('Not an array of files');
+                /* NOT REACHED */
+            }
             $this->far = $_FILES[$name];
 /*
  *  The 'error' sub-array is just used an iteration control and any of the field arrays could be used.
@@ -33,7 +40,7 @@
  *
  * This returns an array that looks like what you would get if the file upload was a
  * singleton and not an array. This can simplify coding in certain circumstances even
- * though it invovles restructuring the data and hence is slightly inefficient. Basically
+ * though it involves restructuring the data and hence is slightly inefficient. Basically
  * the $_FILES array should be made from objects with fields not arrays of keyed values!!!
  *
  * @return array
