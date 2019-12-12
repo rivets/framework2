e201e5a9c8b8a2bcf0ca1cd8c1d7526973c87f01
rivets <lindsay@marshall.name>
2019-12-12 13:41:36 +0000

    get brackets right in comments

--- class/support/ajax.php
+++ class/support/ajax.php
@@ -46,7 +46,7 @@
  * @var array   Values controlling whether or not calls on the bean operation are allowed
  */
         private static $allowBean = [
-            // [[['ContextName', 'RoleName']], [ 'bean' => [...fields...], ...] // an array of roles required in form [['context name', 'role name']...] (can be empty)
+            // [[['ContextName', 'RoleName']], [ 'bean' => [...fields...], ...]] // an array of roles required in form [['context name', 'role name']...] (can be empty)
         ];
 /**
  * @var array   Values controlling whether or not calls on the toggle operation are allowed
@@ -58,13 +58,13 @@
  * @var array   Values controlling whether or not calls on the table operation are allowed
  */
         private static $allowTable = [
-            // [[['ContextName', 'RoleName']], [ 'bean', ....] // an array of roles required in form [['context name', 'role name']...] (can be empty)
+            // [[['ContextName', 'RoleName']], [ 'bean', ....]] // an array of roles required in form [['context name', 'role name']...] (can be empty)
         ];
 /**
  * @var array   Values controlling whether or not calls on the table operation are allowed
  */
         private static $allowTSearch = [
-            // [[['ContextName', 'RoleName']], [ 'bean' => [...fields...], ...] // an array of roles required in form [['context name', 'role name']...] (can be empty)
+            // [[['ContextName', 'RoleName']], [ 'bean' => [...fields...], ...]] // an array of roles required in form [['context name', 'role name']...] (can be empty)
         ];
 /**
  * @var array   Values controlling whether or not bean operations are logged for certain beans
