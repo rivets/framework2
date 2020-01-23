d0f3c835177fd99763b4abbe9a3ab1129f8df82f
rivets <lindsay@marshall.name>
2020-01-23 17:08:43 +0000

    namespace it all

--- assets/js/testajax.js
+++ assets/js/testajax.js
@@ -1,95 +1,98 @@
-    var ajaxops = [ 'bean', 'config', 'hints', 'paging', 'pwcheck', 'shared', 'table', 'tablecheck', 'tablesearch', 'toggle', 'unique', 'uniquenl'];
+    var testing = {
 
-    function makecall(url, data, succ, err)
-    {
-        data.async = true;
-        $.ajax(base+'/ajax/'+url, data).done(succ).fail(err);
-    }
-    function testbean()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Bean operation test complete');
-    }
+        ajaxops : [ 'bean', 'config', 'hints', 'paging', 'pwcheck', 'shared', 'table', 'tablecheck', 'tablesearch', 'toggle', 'unique', 'uniquenl'];
 
-    function testconfig()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Config operation test complete');
-    }
-
-    function testhints()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Hints operation test complete');
-    }
-
-    function testpaging()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Paging operation test complete');
-    }
-
-    function testpwcheck()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Pwcheck operation test complete');
-    }
-
-    function testshared()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Shared operation test complete');
-    }
-
-    function testtable()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Table operation test complete');
-    }
-
-    function testtablecheck()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Tablecheck operation test complete');
-    }
-
-    function testtablesearch()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Tablesearch operation test complete');
-    }
-
-    function testtoggle()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Toggle operation test complete');
-    }
-
-    function testunique()
-    {
-        let t = $(this).parent();
-        bootbox.alert('Unique operation test complete');
-    }
-
-    function testuniquenl()
-    {
-        let t = $(this).parent();
-        makecall('uniquenl/user/login/'+goodlogin, { method: 'GET' }, function(e){
-            t.append('<p>Existing login fails - 200 on existing login</p>');
-        }, function(jx){
-            if (jx.status == 404)
-            {
-               t.append('<p>Existing login OK</p>');
-            }
-            else
-            {
-                t.append('<p>Existing login fails - '+jx.status+'</p>'+jx.responseText);
-            }
-        });
-        makecall('uniquenl/user/login/'+goodlogin+'XXXXX', { method: 'GET' }).done(function(e){
-            t.append('<p>Non-existent login OK</p>');
-        }, function(jx){
-            t.append('<p>Non-existent login fails - '+jx.status+'</p>'+jx.responseText);
-        });
-        t.append('<p>Test Complete</p>');
-    }
+        makecall : function (url, data, succ, err)
+        {
+            data.async = true;
+            $.ajax(base+'/ajax/'+url, data).done(succ).fail(err);
+        }
+        testbean: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Bean operation test complete');
+        }
+    
+        testconfig : function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Config operation test complete');
+        }
+    
+        testhints: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Hints operation test complete');
+        }
+    
+        testpaging: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Paging operation test complete');
+        }
+    
+        testpwcheck: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Pwcheck operation test complete');
+        }
+    
+        testshared: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Shared operation test complete');
+        }
+    
+        testtable: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Table operation test complete');
+        }
+    
+        testtablecheck: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Tablecheck operation test complete');
+        }
+    
+        testablesearch: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Tablesearch operation test complete');
+        }
+    
+        testoggle : function()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Toggle operation test complete');
+        }
+    
+        testunique: function ()
+        {
+            let t = $(this).parent();
+            bootbox.alert('Unique operation test complete');
+        }
+    
+        testuniquenl :function ()
+        {
+            let t = $(this).parent();
+            makecall('uniquenl/user/login/'+goodlogin, { method: 'GET' }, function(e){
+                t.append('<p>Existing login fails - 200 on existing login</p>');
+            }, function(jx){
+                if (jx.status == 404)
+                {
+                   t.append('<p>Existing login OK</p>');
+                }
+                else
+                {
+                    t.append('<p>Existing login fails - '+jx.status+'</p>'+jx.responseText);
+                }
+            });
+            makecall('uniquenl/user/login/'+goodlogin+'XXXXX', { method: 'GET' }).done(function(e){
+                t.append('<p>Non-existent login OK</p>');
+            }, function(jx){
+                t.append('<p>Non-existent login fails - '+jx.status+'</p>'+jx.responseText);
+            });
+            t.append('<p>Test Complete</p>');
+        }
+    };
