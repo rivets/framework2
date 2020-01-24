9c42aa64cf2e8617f0be6952ddce0c24081be757
rivets <lindsay.marshall@ncl.ac.uk>
2020-01-24 16:58:31 +0000

    Psalm

--- ajax.php
+++ ajax.php
@@ -14,6 +14,6 @@
     \Config\Framework::initialise();
 
     // Ajax on, debug on, load twig, load RB
-    $ld = \Framework\Local::getinstance()->setup(__DIR__, TRUE, TRUE, TRUE, TRUE); # setup the Local singleton
+    \Framework\Local::getinstance()->setup(__DIR__, TRUE, TRUE, TRUE, TRUE); # setup the Local singleton
     \Support\Ajax::getinstance()->handle(\Support\Context::getinstance()->setup());
 ?>
