9b09f052bb432c8f70e08bd8dbf7eca4d11467e3
rivets <lindsay.marshall@ncl.ac.uk>
2020-06-16 11:02:28 +0100

    Add some boiler plate code that is often useful

--- devel/hack.php
+++ devel/hack.php
@@ -2,5 +2,17 @@
 /**
  * You can put arbitrary code in here and run it
  */
+    \R::freeze(FALSE); // unfreeze the database in case you want to add fields
+    $context = \Support\Context::getinstance();
+    $siteinfo = \Support\SiteInfo::getinstance();
+    $log = '';
+    $raw = FALSE;
+/******************************************************************************/
+/*
+ * Your code in here. Concatenate output to $log and it will be shown.
+ */
+
+/******************************************************************************/
+    $context->local()->addval(['errlog' => $log, 'raw' => $raw]);
     \Support\Context::getinstance()->local()->message(\Framework\Local::MESSAGE, 'Done');
 ?>
