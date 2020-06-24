b9a4538c090206c4a62f819624c61887533dfaac
rivets <lindsay.marshall@ncl.ac.uk>
2020-06-24 12:42:56 +0100

    Make public

--- class/framework/local.php
+++ class/framework/local.php
@@ -383,7 +383,7 @@
  *
  * @return void
  */
-        private function earlyFail(string $title, string $msg)
+        public function earlyFail(string $title, string $msg)
         {
             if (is_object($this->twig))
             { # we have twig so can render a template
