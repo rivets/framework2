02af4bd6b4f18c5e5d0ff1456bb34e27085523d7
rivets <lindsay.marshall@ncl.ac.uk>
2020-06-29 18:14:44 +0100

    Chnages

--- runpsalm.sh
+++ runpsalm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./vendor/vimeo/psalm/psalm --config=psalm.xml --show-info --find-dead-code
-./vendor/vimeo/psalm/psalm --config=ipsalm.xml --show-info --find-dead-code
+./vendor/vimeo/psalm/psalm --config=psalm.xml --show-info=true --find-dead-code=true
+./vendor/vimeo/psalm/psalm --config=ipsalm.xml --show-info=true --find-dead-code=true
