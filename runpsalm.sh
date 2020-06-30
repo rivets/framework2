e497286b37ec3f2dfb6e56b04deb32dd198a8e88
rivets <lindsay.marshall@ncl.ac.uk>
2020-06-30 10:47:32 +0100

    typo

--- runpsalm.sh
+++ runpsalm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./vendor/vimeo/psalm/psalm --noprogress --stats --clear-cache --find-unused-psalm-suppress --config=psalm.xml --show-info=true --find-dead-code=true --taint-analysis
-./vendor/vimeo/psalm/psalm --noprogress --stats --clear-cache --config=ipsalm.xml --find-unused-psalm-suppress --show-info=true --find-dead-code=true --taint-analysis
+./vendor/vimeo/psalm/psalm --no-progress --stats --clear-cache --find-unused-psalm-suppress --config=psalm.xml --show-info=true --find-dead-code=true --taint-analysis
+./vendor/vimeo/psalm/psalm --no-progress --stats --clear-cache --config=ipsalm.xml --find-unused-psalm-suppress --show-info=true --find-dead-code=true --taint-analysis
