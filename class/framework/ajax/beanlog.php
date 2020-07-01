6e0b8185340d0d26c3bbb82f8a2e0a906450e900
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-01 16:23:59 +0100

    namespace

--- class/framework/ajax/beanlog.php
+++ class/framework/ajax/beanlog.php
@@ -5,7 +5,7 @@
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
  * @copyright 2020 Newcastle University
  */
-    namespace \Framework\Ajax;
+    namespace Framework\Ajax;
 
     use \Support\Context;
 /**
