2e76fc77c3227819c9946af6ec2ffe42e3f99da2
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-01 15:26:21 +0100

    suppress

--- class/pages/nopage.php
+++ class/pages/nopage.php
@@ -21,7 +21,7 @@
  * @param Context   $context    The context object for the site
  *
  * @return string|array<string>     A template name
- * @phpcsSuppress PHP_CodeSniffer.CodeAnalysis.UselessOverridingMethod
+ * @phpcsSuppress PHP_CodeSniffer.Standards.Generic.CodeAnalysis.UselessOverridingMethod
  */
         public function handle(Context $context)
         {
