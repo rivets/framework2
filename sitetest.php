adf52570df9820d4f305062aabb6e9c967376e2e
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-02 21:14:13 +0100

    Update sitetest.php

--- sitetest.php
+++ sitetest.php
@@ -28,9 +28,9 @@
                 ['/nosuchpage', 404, 0, ''],
                 ['/nosuchpage/', 404, 0, ''],
                 ['/admin', 200, 1, '/login?page=/admin'],
-                ['/admin/', 200, 1,  '/login?page=/admin/'],
-                ['/test', 200, 1, '/login?page=/test'],
-                ['/test/', 200, 1,  '/login?page=/test/'],
+                ['/admin/', 200, 1,  '/login/?page=/admin/'],
+                ['/test', 200, 1, '/login/?page=/test'],
+                ['/test/', 200, 1,  '/login/?page=/test/'],
             ]
        ];
 
@@ -54,13 +54,22 @@
         $url = $prefix.$url;
         $data = Curl::fetch($url);
         $info = Curl::code();
-        if ($info['http_code'] != $code || $info['redirect_count'] != $rdcount || ($rdcount > 0 && $prefix.$rdurl != urldecode($info['redirect_url'])))
+        $resurl = urldecode($info['redirect_url'] !== '' ? $info['redirect_url'] : $info['url']);
+        if ($info['http_code'] != $code)
         {
-            echo '"'.$url.'" ('.urldecode($info['url']).') returns '.$info['http_code'].', '.$info['redirect_count'].PHP_EOL;
+            echo '"'.$url.'" ('.$resurl.') returns '.$info['http_code'].' with '.$info['redirect_count'].' redirects'.PHP_EOL;
+        }
+        elseif ($info['redirect_count'] != $rdcount)
+        {
+            echo '"'.$url.'" ('.$resurl.') has '.$info['redirect_count'].' redirects, expecting '.$rdcount.PHP_EOL;
+        }
+        elseif ($rdcount > 0 && $prefix.$rdurl != $resurl)
+        {
+            echo '"'.$url.'" ('.$resurl.') expecting '.$prefix.$rdurl.PHP_EOL;
         }
         elseif ($verbose !== FALSE)
         {
-            echo '"'.$url.'" '.$info['http_code'].' ('.urldecode($info['url']).') OK'.PHP_EOL;
+            echo '"'.$url.'" '.$info['http_code'].' ('.$resurl.') OK'.PHP_EOL;
         }
     }
 ?>
