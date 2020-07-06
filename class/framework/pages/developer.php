d0618576c12b80898ebde3d14d905873709550c3
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-06 11:14:37 +0100

    Restructure developer tools.
    And add start of AJAX configuration

--- class/framework/pages/developer.php
+++ class/framework/pages/developer.php
@@ -29,84 +29,30 @@
             $rest = $context->rest();
             switch ($rest[0])
             {
-            case 'assert': // failed assertion
-                assert(TRUE == FALSE);
+            case 'ajax': // configure user AJAX functions
+                $tpl = '@devel/ajax.twig';
                 break;
 
-            case 'hack': # execute some code.
+            case 'hack': // execute some code.
                 \R::freeze(FALSE); // turn off freezing so that you can fiddle with the database....
                 /** @psalm-suppress UnresolvableInclude */
                 include $context->local()->makebasepath('devel', 'hack.php');
                 break;
-
-            case 'test': # generate a test page
-                $context->local()->message(\Framework\Local::ERROR, 'Error 1');
-                $context->local()->message(\Framework\Local::ERROR, 'Error 2');
-                $context->local()->message(\Framework\Local::WARNING, 'Warning 1');
-                $context->local()->message(\Framework\Local::WARNING, 'Warning 2');
-                $context->local()->message(\Framework\Local::MESSAGE, 'Message 1');
-                $context->local()->message(\Framework\Local::MESSAGE, 'Message 2');
-                $tpl = '@devel/test.twig';
-                break;
-
-            case 'ajax': # test the ajax calls
-                $context->local()->addval('bean', \R::findOrCreate(FW::TEST, ['f1' => 'a string', 'tog' => 1]));
-                $tpl = '@devel/testajax.twig';
-                break;
-
-            case 'upload' :
-                $tpl = '@devel/testupload.twig';
-                $fd = $context->formdata();
-                try
+            
+            case 'test':
+                $test = new \Framework\Support\Test();
+                $rest = $context->rest();
+                if (count($rest) > 1)
                 {
-                    if ($fd->hasfile('upload'))
+                    if (method_exists($test, $rest[1]))
                     {
-                        $upl = \R::dispense('upload');
-                        $upl->savefile($context, $fd->filedata('upload'), FALSE, $context->user(), 0);
-                        $context->local()->addval('download', $upl->getID());
+                        $tpl = $test->{$rest[1]}($context);
                     }
-                    if (count($rest) == 3)
+                    else
                     {
-                        $id = (int) $rest[2];
-                        switch ($rest[1])
-                        {
-                        case 'get':
-                            $context->local()->addval('download', $id);
-                            break;
-
-                        case'delete':
-                            \R::trash($context->load('upload', $id));
-                            $context->local()->message(\Framework\Local::MESSAGE, 'Deleted');
-                            break;
-                        }
+                        $context->web()->bad();
+                        /* not reached */
                     }
-                }
-                catch (\Exception $e)
-                {
-                    $context->local()->message(\Framework\Local::ERROR, $e->getmessage());
-                }
-                break;
-
-            case 'fail': # this lets you test error handling
-                2 / 0;
-                break;
-
-            case 'throw': # this lets you test exception handling
-                throw new \Exception('Unhandled Exception Test');
-
-            case 'mail' : # this lets you test email sending
-/**
- * @psalm-suppress PossiblyNullPropertyFetch
- * @psalm-suppress PossiblyNullArgument
- **/
-                $msg = $context->local()->sendmail([$context->user()->email], 'test', 'test');
-                if ($msg === '')
-                {
-                    $context->local()->message(\Framework\Local::MESSAGE, 'sent');
-                }
-                else
-                {
-                    $context->local()->message(\Framework\Local::ERROR, $msg);
                 }
                 break;
             }
---
+++ class/framework/support/test.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Contains definition of Test class
+ *
+ * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
+ * @copyright 2020 Newcastle University
+ */
+    namespace Framework\Support;
+
+    use \Support\Context;
+/**
+ * A class that handles various site testing related things
+ */
+    class Test
+    {
+/**
+ * Test AJAX functions
+ *
+ * @param Context $context  The site context object
+ *
+ * @return string
+ */
+        public function ajax(Context $context) : string
+        {
+            $context->local()->addval('bean', \R::findOrCreate(FW::TEST, ['f1' => 'a string', 'tog' => 1]));
+            return '@devel/testajax.twig';
+        }
+/**
+ * Test failed assertion handling
+ *
+ * @param Context $context  The site context object
+ *
+ * @return void
+ */
+        public function assert(Context $context) : void
+        {
+            assert(TRUE == FALSE);
+        }
+/**
+ * Test run time error handling
+ *
+ * @param Context $context  The site context object
+ *
+ * @return int
+ */
+        public function fail(Context $context) : int
+        {
+            return 2 / 0;
+        }
+/**
+ * Test mail
+ *
+ * @param Context $context  The site context object
+ *
+ * @return string
+ */
+        public function mail(Context $context) : string
+        {
+/**
+ * @psalm-suppress PossiblyNullPropertyFetch
+ * @psalm-suppress PossiblyNullArgument
+ **/
+            $msg = $context->local()->sendmail([$context->user()->email], 'test', 'test');
+            if ($msg === '')
+            {
+                $context->local()->message(\Framework\Local::MESSAGE, 'sent');
+            }
+            else
+            {
+                $context->local()->message(\Framework\Local::ERROR, $msg);
+            }
+        }
+/**
+ * Generate a test page. This tests various twig macros etc.
+ *
+ * @param Context $context  The site context object
+ *
+ * @return string
+ */
+        public function page(Context $context) : string
+        {
+            $context->local()->message(\Framework\Local::ERROR, 'Error 1');
+            $context->local()->message(\Framework\Local::ERROR, 'Error 2');
+            $context->local()->message(\Framework\Local::WARNING, 'Warning 1');
+            $context->local()->message(\Framework\Local::WARNING, 'Warning 2');
+            $context->local()->message(\Framework\Local::MESSAGE, 'Message 1');
+            $context->local()->message(\Framework\Local::MESSAGE, 'Message 2');
+            return '@devel/test.twig';
+        }
+/**
+ * Throw an unhandled exception. Tests exception handling.
+ *
+ * @param Context $context  The site context object
+ *
+ * @throws \Exception
+ * @return void
+ */
+        public function toss(Context $context) : void
+        {
+            throw new \Exception('Unhandled Exception Test');
+        }
+/**
+ * Test the upload features
+ *
+ * @param Context $context  The site context object
+ *
+ * @return string
+ */
+        public function upload(Context $context) : string
+        {
+            $fd = $context->formdata();
+            try
+            {
+                if ($fd->hasfile('upload'))
+                {
+                    $upl = \R::dispense('upload');
+                    $upl->savefile($context, $fd->filedata('upload'), FALSE, $context->user(), 0);
+                    $context->local()->addval('download', $upl->getID());
+                }
+                if (count($rest) == 3)
+                {
+                    $id = (int) $rest[2];
+                    switch ($rest[1])
+                    {
+                    case 'get':
+                        $context->local()->addval('download', $id);
+                        break;
+
+                    case'delete':
+                        \R::trash($context->load('upload', $id));
+                        $context->local()->message(\Framework\Local::MESSAGE, 'Deleted');
+                        break;
+                    }
+                }
+            }
+            catch (\Exception $e)
+            {
+                $context->local()->message(\Framework\Local::ERROR, $e->getmessage());
+            }
+            return '@devel/testupload.twig';
+        }
+    }
+?>
