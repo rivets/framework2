7b95b5d15cc5c60eab6ddc1e8c6914beffbb104f
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-09 17:51:12 +0100

    param order

--- class/framework/support/testsupport.php
+++ class/framework/support/testsupport.php
@@ -27,13 +27,13 @@
  */
         private function test(string $func, array $params, $result, bool$throwOK) : bool
         {
-            $msg = $func.'('.implode(', ', array_map($params, function($ix, $v){
+            $msg = $func.'('.implode(', ', array_map(function($ix, $v){
                 if (is_array($v))
                 {
                     return '['.implode(', ', $v).']';
                 }
                 return $v;
-            })).')';
+            }), $params).')';
             try
             {
                 $res = $this->fdt->{$func}(...$params);
