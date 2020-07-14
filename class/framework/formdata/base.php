1906cf2601aa7a2c00503cd896806e9cfe9b197a
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-14 17:22:03 +0100

    Nasty use of empty
    But it covers the cases I want covered.

--- class/framework/formdata/base.php
+++ class/framework/formdata/base.php
@@ -136,7 +136,7 @@
                     if (!is_array($part))
                     {
                         $part = trim($part);
-                        if ($filter !== NULL)
+                        if (!empty($filter))
                         {
                             $part = filter_var($part, $filter, $options);
                             if ($part === FALSE || $part === NULL)
