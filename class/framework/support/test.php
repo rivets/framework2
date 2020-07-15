e22ac07395710dca0bdc43c178c4a305049468b4
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-15 11:23:55 +0100

    Tweaks

--- class/framework/support/test.php
+++ class/framework/support/test.php
@@ -14,7 +14,7 @@
  */
     class Test
     {    
-        static private $tests = [ // function, paramters, expected result, if TRUE then failure is expected and result may be default or an exception
+        static private $tests = [ // function, paramters, expected result, if FALSE then failure is expected and result may be default or an exception
             ['exists', ['exist'], TRUE, TRUE],
             ['exists', ['notexist'], FALSE, FALSE],
             ['mustExist', ['exist'], TRUE, TRUE],
