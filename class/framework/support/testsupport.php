923c8b27e28bb6edcd36a817d9a09d1422560c8e
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-15 11:44:10 +0100

    Tweaks

--- class/framework/support/testsupport.php
+++ class/framework/support/testsupport.php
@@ -33,7 +33,7 @@
         private function display($pars, $all = FALSE)
         {
             $x = preg_replace('/array\(/', '[', preg_replace('/,\)/', ']', preg_replace('/\d=>/', '', preg_replace('/\s+/ims', '', var_export($pars, TRUE)))));
-            return preg_replace('/,/', ', ', $all ? $x : substr($x, 1, strlen($x)-2));
+            return preg_replace('/\[\)/', '[]', preg_replace('/,/', ', ', $all ? $x : substr($x, 1, strlen($x)-2)));
         }
 /**
  * Run tests specified
