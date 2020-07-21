575d54af6436613dd9dcfcb3dfc605d9da612eb6
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-21 11:31:03 +0100

    Update for new style

--- class/support/ajax.php
+++ class/support/ajax.php
@@ -13,14 +13,7 @@
     final class Ajax extends \Framework\Ajax
     {
 /**
- * Add functions that implement your AJAX operations here and register them
- * in the handle method below.
- */
-/*
-        public function yourop(Context $context)
-        {
-            // your code
-        }
+ * Add functions that implement your AJAX operations as class in clss/framework/ajax. See the sample.txt file there.
  */
 /**
  * If you are using the predefined features of the framework then you may need to
@@ -86,10 +79,6 @@
         public function handle(Context $context) : void
         {
             parent::__construct();
-/* ***** Your code goes here  ***** */
-            //$this->operation(['yourop', ...], [TRUE, [['ContextName', 'RoleName'],...]]);
-            // TRUE if login needed, then an array of roles required in form [['context name', 'role name']...] (can be empty)
-/* ****** do not change below here ***** */
             $this->access->pageOrHint(self::$allowPaging, self::$allowHints);
             $this->access->beanAccess(self::$allowBean, self::$allowToggle, self::$allowTable, self::$audit, self::$allowTSearch, self::$allowUniquenl);
             parent::handle($context);
