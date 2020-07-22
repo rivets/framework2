aed8f881ad5d4bcf48c4d337e937bfb55f4394cf
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-22 14:34:26 +0100

    Update toggle.php

--- class/framework/ajax/toggle.php
+++ class/framework/ajax/toggle.php
@@ -24,7 +24,7 @@
             FW::ROLECONTEXT => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], ['fixed'] ],
             FW::ROLENAME    => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], ['fixed'] ],
             // FW::TABLE       => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], [] ],
-            FW::TEST        => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], ['toggle'] ], // table does not always exist
+            FW::TEST        => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], ['tog'] ], // table does not always exist
             FW::USER        => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], ['active', 'confirm', FW::ADMIN, FW::DEVELOPER] ], // the latter are special see below
         ];
 /**
