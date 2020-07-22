e069b4cb476025b3763b530d6af7a070d1b8fb15
rivets <lindsay.marshall@ncl.ac.uk>
2020-07-22 11:08:49 +0100

    Update unique.php

--- class/framework/ajax/unique.php
+++ class/framework/ajax/unique.php
@@ -41,7 +41,7 @@
  */
         final public function handle() : void
         {
-            [$bean, $field, $value] = $this->context->restcheck(3);
+            [$bean, $field, $value] = $this->restcheck(3);
             $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $bean, $field);
             if (\R::count($bean, preg_replace('/[^a-z0-9_]/i', '', $field).'=?', [$value]) > 0)
             {
