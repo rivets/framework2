5e72a413e43c020be6732df1ae3dc090d7937d11
rivets <lindsay.marshall@ncl.ac.uk>
2020-10-08 12:45:52 +0100

    Documentation

--- class/model/page.php
+++ class/model/page.php
@@ -2,9 +2,12 @@
 /**
  * A model class for the RedBean object Page
  *
+ * This is a Framework system class - do not edit!
+ *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
  * @copyright 2017-2020 Newcastle University
  * @package Framework
+ * @subpackage SystemModel
  */
     namespace Model;
 
--- class/model/role.php
+++ class/model/role.php
@@ -2,9 +2,12 @@
 /**
  * A model class for the RedBean object Role
  *
+ * This is a Framework system class - do not edit!
+ *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
  * @copyright 2015-2020 Newcastle University
  * @package Framework
+ * @subpackage SystemModel
  */
     namespace Model;
 
