6ea5b26d3297e4e67b224f6a786e0b6f748a2c33
rivets <lindsay.marshall@ncl.ac.uk>
2021-01-02 17:14:02 +0000

    Update install.php

--- install.php
+++ install.php
@@ -124,6 +124,10 @@
     {
         echo '<h2>There has been an installer system exception</h2>';
         echo '<p>'.$e->getMessage().'</p>';
+        ob_start();
+        debug_print_backtrace(1, 2);
+        $back .= ob_get_clean(); // will get used later in make500
+        echo str_replace(',[', ',<br/>&nbsp;&nbsp;&nbsp;&nbsp;[', str_replace(PHP_EOL, '<br/>'.PHP_EOL, htmlentities($back))).'</pre>';
         cleanup();
         exit;
         /** NOT REACHED **/
