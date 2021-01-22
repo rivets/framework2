dbc187b939304f30b241775c06424c2ece338852
rivets <lindsay.marshall@ncl.ac.uk>
2021-01-22 17:00:21 +0000

    A bit of refactoring and add * operation for PUT/PATCH
    Now you can send a load of updates not just one at a time.

--- class/framework/ajax/bean.php
+++ class/framework/ajax/bean.php
@@ -11,6 +11,7 @@
 
     use \Config\Framework as FW;
     use \Framework\Exception\BadOperation;
+    use \Framework\Exception\BadValue;
     use \R;
 /**
  * Operations on beans
@@ -34,96 +35,154 @@
 /**
  * @var string
  */
-        private $class = '';
+        private $model = '';
+/**
+ * Generate a no content, created or call the ajaxResult method on a bean if it exists in its Model
+ *
+ * @param \RedBean\OODBBean $bean       The bean
+ * @param string            $method     The method used to get here
+ *
+ * @return void
+ */
+        private function ajaxResult(\RedBeanPHP\OODBBean $bean, string $method)
+        {
+/*
+ * @psalm-suppress RedundantCondition
+ * @psalm-suppress ArgumentTypeCoercion
+ */
+            if (method_exists($this->model, 'ajaxResult'))
+            {
+                $bean->ajaxResult($this->context, $method, 'bean');
+            }
+            elseif ($method == 'post')
+            { // this is a creation of a bean
+                $this->context->web()->created($bean->getID()); // 201 return code
+            }
+            else
+            {
+                $this->context->web()->noContent();
+            }
+        }
+
 /**
  *  make a new one /ajax/bean/KIND/
+ *
+ * @param string        $beanType  The bean type
+ * @param array<string> $rest      The rest of the URL
+ * @param bool          $log       If TRUE then log the changes
  *
  * @return void
  * @psalm-suppress UnusedMethod
  * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
  * @phpcsSuppress SlevomatCodingStandard.Functions.UnusedParameter
  */
-        private function post(string $bean, array $rest, bool $log) : void
+        private function post(string $beanType, array $rest, bool $log) : void
         {
-            $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $bean);
+            $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $beanType);
 /*
  * @psalm-suppress RedundantCondition
  * @psalm-suppress ArgumentTypeCoercion
  */
-            if (!method_exists($this->class, 'add'))
+            if (!method_exists($this->model, 'add'))
             { // operation not supported
-                throw new BadOperation('Cannot add a '.$bean);
+                throw new BadOperation('Cannot add a '.$beanType);
             }
             /** @psalm-suppress InvalidStringClass */
-            $id = $this->class::add($this->context)->getID();
+            $bean = $this->model::add($this->context);
             if ($log)
             {
-                BeanLog::mklog($this->context, BeanLog::CREATE, $bean, $id, '*', NULL);
+                BeanLog::mklog($this->context, BeanLog::CREATE, $bean, '*', NULL);
             }
-            $this->context->web()->created($id); // 201 return code
+            $this->ajaxResult($bean, 'post');
         }
 /**
  * update a field   /ajax/bean/KIND/ID/FIELD/[FN]
  *
+ * If FIELD has the value * then the formdata has several inputs named by field names, if it is a field
+ * name then the form contains a single field called value
+ *
+ * @param string        $beanType  The bean type
+ * @param array<string> $rest      The rest of the URL
+ * @param bool          $log       If TRUE then log the changes
+ * @param string        $method    patch or put - only needed because we are sharing put and patch because of bad routers.
+ *
  * @return void
  * @psalm-suppress UnusedMethod
  * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
  */
-        private function patch(string $bean, array $rest, bool $log) : void
+        private function patch(string $beanType, array $rest, bool $log, string $method = 'patch') : void
         {
+            $perms = $this->controller->permissions(static::class, self::$permissions);
+            $user = $this->context->user();
             [$id, $field] = $rest;
+            $fdt = $this->context->formdata('put');
             $more = $rest[2] ?? NULL;
-            $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $bean, $field);
-            $bn = $this->context->load($bean, (int) $id, TRUE);
-            $old = $bn->{$field};
-            $bn->{$field} = empty($more) ? $this->context->formdata('put')->mustFetch('value') : $bn->{$more[0]}($this->context->formdata('put')->mustFetch('value'));
-            R::store($bn);
+            $bean = $this->context->load($beanType, (int) $id, TRUE);
+            $old = [];
+            foreach ($field == '*' ? $fdt->fetchAll() : [$field => $fdt->mustFetch('value')] as $fname => $value)
+            {
+                if (is_array($value))
+                { // we are looking at the form here so make sure there is no attempt to pass an array value
+                    throw new BadValue('Arrays are not supported');
+                }
+                this->checkAccess($this->context->user(), $perms, $beanType, $fname); // can we access this field?
+                if (!\Support\Siteinfo::hasField($beanType, $fname))
+                { // we are looking at the form here so make sure there is no attempt to pass an array value
+                    throw new BadValue($beanType.' has no such field as '.$fname);
+                }
+                $old[$fname] = $bean->{$fname};
+                $bean->{$fname} = is_null($more) ? $value : $bean->{$more[0]}($value);
+            }
+            R::store($bean);
             if ($log)
             {
-                BeanLog::mklog($this->context, BeanLog::UPDATE, $bean, $bn->getID(), $field, $old);
+                foreach ($old as $of => $ov)
+                {
+                    BeanLog::mklog($this->context, BeanLog::UPDATE, $beanType, $bean->getID(), $of, $ov);
+                }
             }
-            $this->context->web()->noContent();
+            $this->ajaxResult($bean, $method);
         }
 /**
  * Map put onto patch
  *
- * @return void
- * @psalm-suppress UnusedMethod
- * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
- */
-        private function put(string $bean, array $rest, bool $log) : void
-        {
-            $this->patch($bean, $rest, $log);
-        }
-/**
- * DELETE /ajax/bean/KIND/ID/
+ * @param string        $beanType  The bean type
+ * @param array<string> $rest      The rest of the URL
+ * @param bool          $log       If TRUE then log the changes
  *
  * @return void
  * @psalm-suppress UnusedMethod
  * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
  */
-        private function delete(string $bean, array $rest, bool $log) : void
+        private function put(string $beanType, array $rest, bool $log) : void
         {
-            $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $bean);
+            $this->patch($beanType, $rest, $log, 'patch');
+        }
+/**
+ * DELETE /ajax/bean/KIND/ID/
+ *
+ * @param string        $beanType  The bean type
+ * @param array<string> $rest      The rest of the URL
+ * @param bool          $log       If TRUE then log the changes
+ *
+ * @return void
+ * @psalm-suppress UnusedMethod
+ * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
+ */
+        private function delete(string $beanType, array $rest, bool $log) : void
+        {
+            $this->checkAccess($this->context->user(), $this->controller->permissions(static::class, self::$permissions), $beanType);
             $id = $rest[0] ?? 0; // get the id from the URL
             if ($id <= 0)
             {
-                throw new \Framework\Exception\BadValue('Missing value');
+                throw new BadValue('Missing value');
             }
-            $bn = $this->context->load($bean, (int) $id);
+            $bean = $this->context->load($beanType, (int) $id);
             if ($log)
             {
-                BeanLog::mklog($this->context, BeanLog::DELETE, $bean, (int) $id, '*', json_encode($bn->export()));
+                BeanLog::mklog($this->context, BeanLog::DELETE, $bean, '*', json_encode($bean->export()));
             }
-            /**
-             * @psalm-suppress RedundantCondition
-             * @psalm-suppress ArgumentTypeCoercion
-             */
-            if (method_exists($this->class, 'delete')) // call the clean-up function if it has one
-            {
-                $bn->delete($this->context);
-            }
-            R::trash($bn);
+            R::trash($bean); // If there is a delete function in the model it will get called automatically.
             $this->context->web()->noContent();
         }
 /**
@@ -137,24 +196,24 @@
  */
         final public function handle() : void
         {
-            [$bean, $rest] = $this->restCheck(1);
+            [$beanType, $rest] = $this->restCheck(1);
             $method = strtolower($this->context->web()->method());
             if (!method_exists(self::class, $method))
             {
                 throw new \Framework\Exception\BadOperation($method.' is not supported');
             }
             /** @psalm-suppress UndefinedConstant */
-            $this->class = REDBEAN_MODEL_PREFIX.$bean;
+            $this->model = '\\Model\\'.$beanType;
             /**
              * @psalm-suppress RedundantCondition
              * @psalm-suppress ArgumentTypeCoercion
              */
-            if (method_exists($this->class, 'canAjaxBean'))
-            {
+            if (method_exists($this->model, 'canAjaxBean'))
+            { // permission checking for methods exists for this bean type
                 /** @psalm-suppress InvalidStringClass */
-                $this->class::canAjaxBean($this->context, $method);
+                $this->model::canAjaxBean($this->context, $method);
             }
-            $this->{$method}($bean, $rest, $this->controller->log($bean));
+            $this->{$method}($beanType, $rest, $this->controller->log($beanType));
         }
     }
 ?>
