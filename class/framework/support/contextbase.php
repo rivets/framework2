e4d0ace73afea3a7fdf3fd0fd607d048647fbc65
rivets <lindsay.marshall@ncl.ac.uk>
2021-01-26 14:38:19 +0000

    Gte rid of the old session stuff

--- class/framework/support/contextbase.php
+++ class/framework/support/contextbase.php
@@ -305,15 +305,7 @@
             {# see if there is a user variable in the session....
                 /** @psalm-suppress UnusedFunctionCall */
                 session_start(['name' => Config::SESSIONNAME]);
-                if (isset($_SESSION['user']))
-                { // deprecated
-                    $this->luser =  $_SESSION['user'];
-                    $this->luser->fresh(); // not taht helpful though as it doesn't clear arrays
-                }
-                elseif (isset($_SESSION['userID']))
-                {
-                    $this->luser =  $this->load(FW::USER, $_SESSION['userID']);
-                }
+                $this->luser =  $this->load(FW::USER, $_SESSION['userID']);
             }
             $this->mtoken();
 
