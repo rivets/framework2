1bc99461887b64a336d403caab35a06c010b7fc6
rivets <lindsay.marshall@ncl.ac.uk>
2021-02-09 09:56:38 +0000

    Add default page size to pageCount

--- class/framework/support/siteinfobase.php
+++ class/framework/support/siteinfobase.php
@@ -123,7 +123,7 @@
  * @return int
  * @psalm-suppress PossiblyUnusedMethod
  */
-        public function pageCount(string $table, int $pagesize, string $where = '', array $pars = []) : int
+        public function pageCount(string $table, int $pagesize = 10, string $where = '', array $pars = []) : int
         {
             $count = R::count($table, $where, $pars);
             return (int) \floor(($count % $pagesize > 0 ? ($count + $pagesize) : $count) / $pagesize);
