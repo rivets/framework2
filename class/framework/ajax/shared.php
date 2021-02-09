29d3bf0c9a914b74d21e50ca1db8b91b49f50f69
rivets <lindsay.marshall@ncl.ac.uk>
2021-02-09 11:00:34 +0000

    Update shared.php

--- class/framework/ajax/shared.php
+++ class/framework/ajax/shared.php
@@ -16,7 +16,7 @@
     class Shared extends Ajax
     {
 /**
- * @var array
+ * @var array<mixed>
  */
         private static $permissions = [
             FW::CONFIG      => [ TRUE, [[FW::FWCONTEXT, FW::ADMINROLE]], [] ],
@@ -31,8 +31,6 @@
         ]; //          [ [Roles], ['BeanName' => [FieldNames - all if empty]]]]
 /**
  * Carry out operations on RB shared lists
- *
- * @param \Support\Context    $context The context object
  *
  * @throws \Framework\Exception\BadOperation
  * @return void
