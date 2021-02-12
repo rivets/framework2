f22b32f26ea9f370fe1ffaef78bc49667ae9d8f1
rivets <lindsay.marshall@ncl.ac.uk>
2021-02-12 15:11:46 +0000

    Update csp.php

--- class/framework/web/csp.php
+++ class/framework/web/csp.php
@@ -201,6 +201,7 @@
                         $x = \implode('.', $x);
                         if (\R::findOne(FW::CSP, 'type=? and host=?', [$type, $x]) === NULL)
                         { // doesn't seem to be in there
+                            var_dump($x, $host, $type); exit;
                             $bn = \R::dispense(\Config\Framework::CSP);
                             $bn->type = $type;
                             $bn->host = $host;
