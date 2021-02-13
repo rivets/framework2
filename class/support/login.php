7a637e564f21cfa3a4f4c33c041137192957c428
rivets <lindsay.marshall@ncl.ac.uk>
2021-02-13 17:17:20 +0000

    Update login.php

--- class/support/login.php
+++ class/support/login.php
@@ -74,16 +74,16 @@
 
             // If it's desired to kill the session, also delete the session cookie.
             // Note: This will destroy the session, and not just the session data!
-            if (ini_get('session.use_cookies'))
+            if (\ini_get('session.use_cookies'))
             {
                 $params = session_get_cookie_params();
                 setcookie(session_name(), '', time() - 42000,
                     $params['path'], $params['domain'], $params['secure'], $params['httponly']
                 );
             }
-            if (session_status() === PHP_SESSION_ACTIVE)
+            if (\session_status() === PHP_SESSION_ACTIVE)
             { // no session started yet
-                session_destroy(); // Finally, destroy the -session.
+                \session_destroy(); // Finally, destroy the -session.
             }
             $context->divert('/');
             /* NOT REACHED */
