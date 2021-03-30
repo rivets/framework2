2f0c967d493f3e40e1d3a0cbad67cd709846188d
rivets <lindsay.marshall@ncl.ac.uk>
2021-03-30 16:21:42 +0100

    Update contextbase.php

--- class/framework/support/contextbase.php
+++ class/framework/support/contextbase.php
@@ -17,7 +17,7 @@
  */
     class ContextBase
     {
-        use \Framework\Utility\Singleton; // phpcs:disable
+        use \Framework\Utility\Singleton; // @phpcsSuppress Import.OrderedImportsFixer
 
 /** @var ?OODBBean  NULL or an object decribing the current logged in User (if we have logins at all) */
         protected $luser        = NULL;
