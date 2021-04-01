8db3822d5ff2ef1bc705a4f7cf3e052cfe2ccdff
rivets <lindsay.marshall@ncl.ac.uk>
2021-04-01 18:12:08 +0100

    Update fw-editable.js

--- assets/js/fw-editable.js
+++ assets/js/fw-editable.js
@@ -1,13 +1,12 @@
  fwdom.makeEdit = function()
  {
-    return '<input type="text" value="' + this.innerText + '"/>';
+    return '<form><input type="text" value="' + this.innerText + '"/></form>';
  };
 
  fwdom.editable = function(e) {
     e.target.style.cursor = 'pointer';
     let popover = new bootstrap.Popover(e.target, {
         title: 'Edit',
-        container: false,
         html: true,
         content: fwdom.makeEdit,
         placement: 'auto',
