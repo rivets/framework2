1ce04eecd38db4a920e744da3a655d0384f1ac8f
rivets <lindsay.marshall@ncl.ac.uk>
2021-04-15 17:17:51 +0100

    Update add2fa.php

--- class/framework/pages/add2fa.php
+++ class/framework/pages/add2fa.php
@@ -26,14 +26,16 @@
  */
         public function handle(Context $context)
         {
+            $user = $context->user();
             if ($context->web()->isPost())
             {
-                $user = $context->user();
+
                 $fdt = $context->formdata('post');
                 if ($fdt->exists('disable'))
                 {
                     $user->secret = '';
                     \R::store($user);
+                    $context->local()->message(\Framework\Local::WARNING, '2-Factor Authentication disabled');
                 }
                 elseif ($fdt->exists('validator'))
                 {
@@ -47,27 +49,17 @@
                     }
                 }
             }
-            else
-            {
-                $user = $context->user();
-                if ($user->secret() !== '')
-                {
-                    if ($context->web()->isPost())
-                    {
-                    }
-                }
-                else
-                { // enabling it
-                    $secret  = \Framework\Support\Security::getinstance()->make2FASecret();
-                    $user->secret = $secret;
-                    \R::store($user);
-                    \ob_start();
-                    \QRcode::png('otpauth://totp/'.$context->local()->configVal('sitename').'/'.$user->login.'/?secret='.$secret, FALSE, QR_ECLEVEL_L, 6);
-                    $stringdata = \ob_get_clean();
-                    $context->local()->addval([
-                        'qrcode' => 'data:image/png;base64,'.base64_encode($stringdata)
-                    ]);
-                }
+            if ($user->secret() === '')
+            { // enabling it
+                $secret  = \Framework\Support\Security::getinstance()->make2FASecret();
+                $user->secret = $secret;
+                \R::store($user);
+                \ob_start();
+                \QRcode::png('otpauth://totp/'.$context->local()->configVal('sitename').'/'.$user->login.'/?secret='.$secret, FALSE, QR_ECLEVEL_L, 6);
+                $stringdata = \ob_get_clean();
+                $context->local()->addval([
+                    'qrcode' => 'data:image/png;base64,'.base64_encode($stringdata)
+                ]);
             }
             return '@util/add2fa.twig';
         }
