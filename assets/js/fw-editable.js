9b7475583ce5aec3334c72936b97504decb51092
rivets <lindsay.marshall@ncl.ac.uk>
2021-04-20 11:11:03 +0100

    update

--- assets/js/fw-editable.js
+++ assets/js/fw-editable.js
@@ -16,6 +16,12 @@
             template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
         });
         popover.show();
-        div.querySelector('input').focus();
+        popover.tip.querySelector('input').focus();
+        popover.tip.querySelector('.edno').addEventListener('click', function(e){
+            popover.dispose();
+        });
+        popover.tip.querySelector('.edyes').addEventListener('click', function(e){
+            popover.dispose();
+        });
     });
  };
