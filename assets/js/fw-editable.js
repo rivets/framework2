4b7ce2ebf5b45e1553aaf10bfdbbc75f034c90c2
rivets <lindsay.marshall@ncl.ac.uk>
2021-04-20 11:39:28 +0100

    update

--- assets/js/fw-editable.js
+++ assets/js/fw-editable.js
@@ -1,6 +1,5 @@
- fwdom.makeEdit = function(d)
+ fwdom.makeEdit = function(d, type)
  {
-    let type = d.getAttribute('data-type');
     let box = '';
     let text = d.innerText;
     if (text === '')
@@ -13,10 +12,10 @@
         box = '<span>Not supported yet<span>';
         break;
     case 'textarea':
-        box = '<textarea rows="5" cols="80">' + text + '"</textarea>';
+        box = '<textarea rows="5" cols="80" class="edbox">' + text + '"</textarea>';
         break;
     default:
-        box = '<input type="'+type+'" value="' + text + '"/>';
+        box = '<input type="'+type+'" value="' + text + '" class="edbox"/>';
         break;
     }
     return box + '<i class="fas fa-times-circle edno"></i><i class="fas fa-check-circle edyes"></i>';
@@ -24,18 +23,23 @@
 
  fwdom.editable = function(div) {
     let name = div.getAttribute('name');
+    let type = div.getAttribute('data-type');
     div.addEventListener('click', function(e){
         let popover = new bootstrap.Popover(div, {
             title: div.getAttribute('data-title'),
             html: true,
             trigger: 'click focus',
             sanitize: false,
-            content: fwdom.makeEdit(div),
+            content: fwdom.makeEdit(div, type),
             placement: 'auto',
             template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
         });
         popover.show();
-        popover.tip.querySelector('input').focus();
+        let box = popover.tip.querySelector('.edBox')
+        box.focus();
+        box.addEventListener('blur', function(e){
+            console.log(e);
+        });
         popover.tip.querySelector('.edno').addEventListener('click', function(e){
             popover.dispose();
         });
