67c2a9adc98043cfb195b6c079ac540352a976a9
rivets <lindsay.marshall@ncl.ac.uk>
2021-04-20 11:03:18 +0100

    update

--- assets/js/fw-editable.js
+++ assets/js/fw-editable.js
@@ -1,7 +1,7 @@
  fwdom.makeEdit = function(d)
  {
-    return '<form><input type="text" value="' + d.innerText + '"/>' +
-       '<i class="fas fa-times-circle edno"></i><i class="fas fa-check-circle edyes"></i></form>';
+    return '<input type="text" value="' + d.innerText + '"/>' +
+       '<i class="fas fa-times-circle edno"></i><i class="fas fa-check-circle edyes"></i>';
  };
 
  fwdom.editable = function(div) {
