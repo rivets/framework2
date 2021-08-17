9ce68c6aaa78f3d74cc6672543bc0e4ed44ad271
rivets <lindsay.marshall@ncl.ac.uk>
2021-08-17 16:54:55 +0100

    Upgrades

--- class/framework/support/errorhandlerbase.php
+++ class/framework/support/errorhandlerbase.php
@@ -50,16 +50,16 @@
  * Set up all the system error handlers
  */
             /** @psalm-suppress ArgumentTypeCoercion */
-            set_exception_handler([$this, 'exceptionHandler']);
-            set_error_handler([$this, 'errorHandler']);
+            \set_exception_handler([$this, 'exceptionHandler']);
+            \set_error_handler([$this, 'errorHandler']);
             /** @psalm-suppress InvalidArgument - psalm doesnt have the right spec for this function */
             /** @psalm-suppress ArgumentTypeCoercion */
-            register_shutdown_function([$this, 'shutdown']);
+            \register_shutdown_function([$this, 'shutdown']);
             if ($devel)
             { // set up expectation handling if in developer mode
-                if (ini_get('zend.assertions') == 1)
+                if (\ini_get('zend.assertions') == 1)
                 {
-                    ini_set('assert.exception', 1);
+                    \ini_set('assert.exception', '1');
 //                    assert_options(\ASSERT_ACTIVE, $devel);
 //                    assert_options(\ASSERT_WARNING, 0);
 //                    assert_options(\ASSERT_CALLBACK, [$this, 'assertFail']);
@@ -96,14 +96,12 @@
  * Rewrite error string
  *
  * @param string $origin HTTP details
- *
- * @return string
  */
         protected function eRewrite(string $origin = '') : string
         {
             return '<pre>'.
-                str_replace(PHP_EOL, '<br/>'.PHP_EOL, htmlentities($origin)).
-                str_replace(',[', ',<br/>&nbsp;&nbsp;&nbsp;&nbsp;[', str_replace(PHP_EOL, '<br/>'.PHP_EOL, htmlentities($this->back))).'</pre>';
+                \str_replace(PHP_EOL, '<br/>'.PHP_EOL, \htmlentities($origin)).
+                \str_replace(',[', ',<br/>&nbsp;&nbsp;&nbsp;&nbsp;[', \str_replace(PHP_EOL, '<br/>'.PHP_EOL, \htmlentities($this->back))).'</pre>';
         }
 /**
  * Tell sysadmin there was an error
@@ -112,20 +110,18 @@
  * @param int|string    $type   An error type
  * @param string        $file   file in which error happened
  * @param int           $line    Line at which it happened
- *
- * @return string
  */
         protected function tellAdmin(string $msg, $type, string $file, int $line) : string
         {
             $this->error = TRUE; // flag that we are handling an error
             $ekey = $file.' | '.$line.' | '.$type.' | '.$msg;
-            $subject = Config::SITENAME.' '.date('c').' System Error - '.$msg.' '.$ekey;
-            $origin = $subject.PHP_EOL.PHP_EOL;
+            $subject = Config::SITENAME.' '.\date('c').' System Error - '.$msg.' '.$ekey;
+            $origin = $subject.\PHP_EOL.\PHP_EOL;
             foreach (self::$tellfields as $fld)
             {
                 if (isset($_SERVER[$fld]))
                 {
-                    $origin .= $fld.': '.$_SERVER[$fld].PHP_EOL;
+                    $origin .= $fld.': '.$_SERVER[$fld].\PHP_EOL;
                 }
             }
             if (!isset($this->senterrors[$ekey]))
@@ -134,19 +130,19 @@
                 if (isset($_GET['fwtrace']))
                 {
                     $this->debug = TRUE;
-                    ob_start();
-                    debug_print_backtrace((int) $_GET['fwtrace'], (int) $_GET['fwdepth'] ?? 0);
-                    $this->back .= ob_get_clean(); // will get used later in make500
+                    \ob_start();
+                    \debug_print_backtrace((int) $_GET['fwtrace'], (int) $_GET['fwdepth'] ?? 0);
+                    $this->back .= \ob_get_clean(); // will get used later in make500
                     if (isset($_GET['fwdump']))
                     { // save the error message to a file in /debug
                         Debug::show($this->back);
                     }
                 }
                 /** @psalm-suppress RedundantCondition */
-                if (Config::USEPHPM || ini_get('sendmail_path') !== '')
+                if (Config::USEPHPM || \ini_get('sendmail_path') !== '')
                 {
                     $err = $this->local->sendmail([Config::SYSADMIN], $subject,
-                        $this->eRewrite($origin), $origin.PHP_EOL.'Type : '.$type.PHP_EOL.$file.' Line '.$line.PHP_EOL.$this->back,
+                        $this->eRewrite($origin), $origin.\PHP_EOL.'Type : '.$type.\PHP_EOL.$file.' Line '.$line.\PHP_EOL.$this->back,
                         ['from' => Config::SITENOREPLY]);
                     if ($err !== '')
                     {
@@ -160,12 +156,10 @@
  * Generate a 500 and possibly an error page
  *
  * @param string $ekey    Error information string
- *
- * @return void
  */
         protected function make500(string $ekey) : void
         {
-            if (!headers_sent())
+            if (!\headers_sent())
             { // haven't generated any output yet.
                 if ($this->devel || !$this->ajax)
                 { // not in an ajax page so try and send a pretty error
