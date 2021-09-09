fdfc4a9acb6742819c6db565e8bc342266d3cb0d
rivets <lindsay.marshall@ncl.ac.uk>
2021-09-09 16:56:36 +0100

    Update user.php

--- class/model/user.php
+++ class/model/user.php
@@ -24,7 +24,7 @@
  */
         private string $roletype = FW::ROLE;
 /**
- * @var array<bool>   Key is name of field and the array contains flags for checks
+ * @var array<array<bool>>  Key is name of field and the array contains flags for checks
  * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
  */
         private static array $editfields = [
