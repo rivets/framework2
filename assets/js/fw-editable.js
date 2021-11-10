f59d5032b34e1ae2893730ecc8cc6c24ea164cd5
rivets <lindsay.marshall@ncl.ac.uk>
2021-11-10 18:04:44 +0000

    Updates

--- assets/js/fw-editable.js
+++ assets/js/fw-editable.js
@@ -1,39 +1,45 @@
-/* globals document, fwdom, bootstrap, alert */
+/* globals document, fwdom, bootstrap */
 /* jshint undef: true, unused: false */
 
 fwdom.popover = null;
 fwdom.inline = null;
 fwdom.edOptions = [];
+fwdom.defaultOptions = {
+    type: 'text',
+    emptytext: '------',
+    title: 'Edit String'
+}
 
-fwdom.makeEdit = function(d, type, text)
+fwdom.makeEdit = function(d)
  {
-    let box = '';
+    const options = fwdom.edOptions[d];
     let ctext = d.innerText;
-    if (text === d.getAttribute('data-emptytext'))
+    let box;
+    if (ctext === options.emptyText)
     {
-        text = '';
+        ctext = '';
     }
-    switch (type)
+    switch (options.type)
     {
     case 'select':
         box = '<select class="edbox">';
-        for (let opt of text.split(';'))
+        for (let opt of options.source)
         {
             box += '<option'+(opt == ctext ? ' selected' : '')+'>'+opt+'</option>';
         }
         box += '</select>';
         break;
     case 'textarea':
-        box = '<textarea rows="5" cols="25" class="edbox">' + text + '</textarea>';
+        box = '<textarea rows="5" cols="25" class="edbox">' + ctext + '</textarea>';
         break;
     default:
-        box = '<input type="'+type+'" value="' + text + '" class="edbox"/>';
+        box = '<input type="'+options.type+'" value="' + ctext + '" class="edbox"/>';
         break;
     }
     return box + '<i class="fas fa-times-circle edno"></i><i class="fas fa-check-circle edyes"></i>';
  };
 
- fwdom.popDispose = function(e)
+ fwdom.popDispose = function()
  {
     document.body.removeEventListener('click', fwdom.outsideClick);
     if (fwdom.popover !== null)
@@ -51,25 +57,38 @@ fwdom.makeEdit = function(d, type, text)
     }
  };
 
- fwdom.editable = function(div, options = {}) {
+ fwdom.editable = function(div, options = null) {
+    if (options != null)
+    {
+        fwdom.edOptions[div] = options;
+    }
+    else
+    {
+        let nopt = fwdom.defaultOptions;
+        let datas = div.dataset;
+        for (let fld in datas)
+        {
+console.log(fld);
+            nopt[fld] = datas[fld];
+        }
+        fwdom.edOptions[div] = nopt;
+    }
     if (div.innerText === '')
     {
         div.innerText = div.getAttribute('data-emptytext');
         div.classList.add('edempty');
     }
-    let name = div.getAttribute('name');
-    fwdom.edOptions[name] = options;
-    let type = div.getAttribute('data-type');
     div.addEventListener('click', function(e){
+        fwdom.stop(e);
         if (fwdom.inline !== null)
         {
             fwdom.popDispose();
         }
         let popover = new bootstrap.Popover(div, {
-            title: this.getAttribute('data-title'),
+            title: fwdom.edOptions[this].title, //this.getAttribute('data-title'),
             html: true,
             sanitize: false,
-            content: fwdom.makeEdit(this, type, this.getAttribute('data-value')),
+            content: fwdom.makeEdit(this),
             placement: 'auto',
             template: '<div class="popover" role="tooltip"><div class="popover-arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
         });
@@ -79,21 +98,26 @@ fwdom.makeEdit = function(d, type, text)
 
         tip.querySelector('.edno').addEventListener('click', fwdom.popDispose);
         tip.querySelector('.edyes').addEventListener('click', function(e){
+            fwdom.stop(e);
             let box = tip.querySelector('.edbox');
             if (box.value != fwdom.inline.innerText)
             {
-                 alert('update');
-                 if (box.value === '')
-                 {
-                    fwdom.inline.innerText = fwdom.inline.getAttribute('data-emptytext');
-                    fwdom.inline.classList.add('edempty');
-                 }
-                 else
-                 {
-                    fwdom.inline.innerText = box.value;
-                    fwdom.inline.classList.remove('edempty');
-                 }
-
+                if (box.value === '')
+                {
+                   fwdom.inline.innerText = fwdom.edOptions[fwdom.inline].emptytext;
+                   fwdom.inline.classList.add('edempty');
+                }
+                else
+                {
+                   fwdom.inline.innerText = box.value;
+                   fwdom.inline.classList.remove('edempty');
+                }
+                if (options.update === null)
+                {
+                    fwdom.alert('No update function defined');
+                    return;
+                }
+                options.update(options, box.value);
             }
             fwdom.popDispose();
         });
