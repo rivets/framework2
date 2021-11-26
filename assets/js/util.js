6cf44fe496311eb4e6cbc26b2e4c43e90abf07a7
rivets <lindsay.marshall@ncl.ac.uk>
2021-11-26 18:19:44 +0000

    update

--- assets/js/util.js
+++ assets/js/util.js
@@ -559,16 +559,10 @@
  * Pop up an alert
  */
         alert: function(message, title = '') {
-            document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="modal" id="_fwalert" tabindex="-1">'+
-                '<div class="modal-dialog">'+
-                '<div class="modal-content">'+
-                '<div class="modal-header">'+
-                '<h5 class="modal-title">'+title+'</h5>'+
-                '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'+
-                '</div>'+
-                '<div class="modal-body">'+
-                '<p>'+message+'</p>'+
-                '</div>'+
+            document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="modal" id="_fwalert" tabindex="-1"><div class="modal-dialog">'+
+                '<div class="modal-content"><div class="modal-header"><h5 class="modal-title">'+title+'</h5>'+
+                '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>'+
+                '<div class="modal-body"><p>'+message+'</p></div>'+
                 '<div class="modal-footer">'+
                 '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">OK</button>'+
                 '</div></div></div></div>');
