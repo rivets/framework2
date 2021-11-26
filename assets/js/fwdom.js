3a847943f4e7990fd6ece579122d52dba80bea47
rivets <lindsay.marshall@ncl.ac.uk>
2021-11-26 18:20:29 +0000

    Update fwdom.js

--- assets/js/fwdom.js
+++ assets/js/fwdom.js
@@ -1,4 +1,4 @@
-/* globals document, framework, bootbox jQuery */
+/* globals document, framework, jQuery */
 /* jshint undef: true, unused: false */
 /*
  * Some utility functions to make getting round the dom a bit less wordy
