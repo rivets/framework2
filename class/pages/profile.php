a35554e54e266fd00a467defab5fa8bb51919f25
rivets <lindsay.marshall@ncl.ac.uk>
2022-01-11 16:25:59 +0000

    profile

--- class/pages/profile.php
+++ class/pages/profile.php
@@ -10,6 +10,7 @@
     namespace Pages;
 
     use \Support\Context;
+    use \Config\Config as Config;
 /**
  * A profile page class
  */
@@ -24,6 +25,44 @@
  */
         public function handle(Context $context) : array|string
         {
+            if ($context->web()->isPost())
+            {
+                $fdt = $context->formdata('post');
+                $user = $context->user();
+                $email = $fdt->fetch('email', '');
+                $change = FALSE;
+                if ($email !== '' && $email != $user->email)
+                {
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL) !== FALSE)
+                    {
+                        $user->email = $email;
+                        $change = TRUE;
+                    }
+                    else
+                    {
+                        $context->local()->message(\Framework\Local::ERROR, 'Please enter a valid email address');
+                    }
+                }
+                $pw = $fdt->fetch('password', '');
+                $rpw = $fdt->fetch('repeat', '');
+                if ($pw !== '')
+                {
+                    if ($pw == $rpw)
+                    {
+                        $user->setpw($pw);
+                        $change = TRUE;
+                    }
+                    else
+                    {
+                        $context->local()->message(\Framework\Local::ERROR, 'Passwords do not match');
+                    }
+                }
+                if ($change)
+                {
+                    \R::store($user);
+                    $context->local()->message(\Framework\Local::MESSAGE, 'Done');
+                }
+            }
             return '@content/profile.twig';
         }
     }
