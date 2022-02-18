388005bf4aa92d611e38f5c56b582911a245ef88
rivets <lindsay.marshall@ncl.ac.uk>
2022-02-18 12:33:54 +0000

    Update update.php

--- class/config/update.php
+++ class/config/update.php
@@ -19,6 +19,7 @@
 /**
  * Apply any updates that need to change the database name etc.
  * Normally will do nothing.
+ * There needs to be some kind of exclusion in place to stop things being applied multiple times!!
  */
         public function apply() : void
         {
