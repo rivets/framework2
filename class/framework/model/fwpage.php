390cc7338340c1fbc331e30901dbf1da15a84d47
rivets <lindsay.marshall@ncl.ac.uk>
2022-05-19 10:37:39 +0100

    move things about

--- class/framework/model/fwpage.php
+++ class/framework/model/fwpage.php
@@ -13,6 +13,7 @@
     use \Config\Framework as FW;
     use \Framework\Dispatch;
     use \Framework\Exception\BadValue;
+    use \Framework\Support\FWPageHelper as Helper;
     use \Support\Context;
 /**
  * A class implementing a RedBean model for Page beans
@@ -92,84 +93,84 @@
                 }
             }
         }
-/**
- * Handle an object page
- *
- * @todo detwig this
- */
-        private static function doObject(Context $context, \RedBeanPHP\OODBBean $page)
-        {
-            if (!\preg_match('/\\\\/', $page->source))
-            { // no namespace so put it in \Pages
-                $page->source = '\\Pages\\'.$page->source;
-                \R::store($page);
-            }
-            $tl = \strtolower($page->source);
-            $tspl = \explode('\\', $page->source);
-            $base = \array_pop($tspl);
-            $lbase = \strtolower($base);
-            $namespace = \implode('\\', \array_filter($tspl));
-            $src = \preg_replace('/\\\\/', \DIRECTORY_SEPARATOR, $tl).'.php';
-            $file = $context->local()->makebasepath('class', $src);
-            if (!\file_exists($file))
-            { // make the file
-                $fd = \fopen($file, 'w');
-                if ($fd === FALSE)
-                {
-                    throw new \Framework\Exception\InternalError('Cannot create PHP file');
-                }
-                $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
-                \fwrite($fd, $context->local()->getRender('@util/pagesample.twig', ['pagename' => $page->name, 'namespace' => $namespace]));
-                \fclose($fd);
-            }
-/**
- * @todo Make the render initialisation value a config value somewhere
- */
-            $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
-            $context->local()->makeTemplate($context, ['content', $lbase.'.twig']); // make a basic twig if there is not one there already.
-        }
-/**
- * Handle a template page
- *
- * @todo detwig this
- */
-        public static function doTemplate(Context $context, \RedBeanPHP\OODBBean $page) : void
-        {
-            if (!\preg_match('/\.twig$/', $page->source))
-            { // doesn't end in .twig
-                $page->source .= '.twig';
-            }
-            else
-            { // sometimes there are extra .twig extensions...
-                $page->source = \preg_replace('/(\.twig)+$/', '.twig', $page->source); // this removes extra .twigs .....
-            }
-            if (!\preg_match('/^@/', $page->source))
-            {
-                if (\preg_match('#/#', $page->source))
-                { // has directory separator characters in it so leave it alone - may be new top-level twig directory.
-                    $name = $page->source;
-                }
-                else
-                { // no namespace so put it in @content
-                    $page->source = '@content/'.$page->source;
-                    $name = ['content', $page->source];
-                    \R::store($page);
-                }
-            }
-            elseif (\preg_match('%^@content/(.*)%', $page->source, $m))
-            { // this is in the User twig content directory
-                $name = ['content', $m[1]];
-            }
-            elseif (\preg_match('%@([a-z]+)/(.*)%', $page->source, $m))
-            { // this is using a system twig
-                $name = ['framework', $m[1], $m[2]];
-            }
-            else
-            {
-                throw new \Framework\Exception\BadValue('Not recognised');
-            }
-            $context->local->makeTemplate($context, $name);
-        }
+///**
+// * Handle an object page
+// *
+// * @todo detwig this
+// */
+//        private static function doObject(Context $context, \RedBeanPHP\OODBBean $page)
+//        {
+//            if (!\preg_match('/\\\\/', $page->source))
+//            { // no namespace so put it in \Pages
+//                $page->source = '\\Pages\\'.$page->source;
+//                \R::store($page);
+//            }
+//            $tl = \strtolower($page->source);
+//            $tspl = \explode('\\', $page->source);
+//            $base = \array_pop($tspl);
+//            $lbase = \strtolower($base);
+//            $namespace = \implode('\\', \array_filter($tspl));
+//            $src = \preg_replace('/\\\\/', \DIRECTORY_SEPARATOR, $tl).'.php';
+//            $file = $context->local()->makebasepath('class', $src);
+//            if (!\file_exists($file))
+//            { // make the file
+//                $fd = \fopen($file, 'w');
+//                if ($fd === FALSE)
+//                {
+//                    throw new \Framework\Exception\InternalError('Cannot create PHP file');
+//                }
+//                $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
+//                \fwrite($fd, $context->local()->getRender('@util/pagesample.twig', ['pagename' => $page->name, 'namespace' => $namespace]));
+//                \fclose($fd);
+//            }
+///**
+// * @todo Make the render initialisation value a config value somewhere
+// */
+//            $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
+//            $context->local()->makeTemplate($context, ['content', $lbase.'.twig']); // make a basic twig if there is not one there already.
+//        }
+///**
+// * Handle a template page
+// *
+// * @todo detwig this
+// */
+//        public static function doTemplate(Context $context, \RedBeanPHP\OODBBean $page) : void
+//        {
+//            if (!\preg_match('/\.twig$/', $page->source))
+//            { // doesn't end in .twig
+//                $page->source .= '.twig';
+//            }
+//            else
+//            { // sometimes there are extra .twig extensions...
+//                $page->source = \preg_replace('/(\.twig)+$/', '.twig', $page->source); // this removes extra .twigs .....
+//            }
+//            if (!\preg_match('/^@/', $page->source))
+//            {
+//                if (\preg_match('#/#', $page->source))
+//                { // has directory separator characters in it so leave it alone - may be new top-level twig directory.
+//                    $name = $page->source;
+//                }
+//                else
+//                { // no namespace so put it in @content
+//                    $page->source = '@content/'.$page->source;
+//                    $name = ['content', $page->source];
+//                    \R::store($page);
+//                }
+//            }
+//            elseif (\preg_match('%^@content/(.*)%', $page->source, $m))
+//            { // this is in the User twig content directory
+//                $name = ['content', $m[1]];
+//            }
+//            elseif (\preg_match('%@([a-z]+)/(.*)%', $page->source, $m))
+//            { // this is using a system twig
+//                $name = ['framework', $m[1], $m[2]];
+//            }
+//            else
+//            {
+//                throw new \Framework\Exception\BadValue('Not recognised');
+//            }
+//            $context->local->makeTemplate($context, $name);
+//        }
 /**
  * Add a Page
  *
@@ -208,10 +209,10 @@
                 switch ($page->kind)
                 {
                 case Dispatch::OBJECT:
-                    self::doObject($context, $page);
+                    Helper::doObject($context, $page);
                     break;
                 case Dispatch::TEMPLATE:
-                    self::doTemplate($context, $page);
+                    Helper::doTemplate($context, $page);
                     break;
                 case Dispatch::REDIRECT:
                 case Dispatch::REHOME:
---
+++ class/framework/support/fwpagehelper.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * A helper class for the RedBean object FWPage
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! This is a Framework system class - do not edit !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
+ * @copyright 2022 Newcastle University
+ * @package Framework\Support
+ */
+    namespace Framework\Support;
+
+    use \Config\Framework as FW;
+    use \Framework\Dispatch;
+    use \Framework\Exception\BadValue;
+    use \Support\Context;
+/**
+ * A class implementing a RedBean model for Page beans
+ * @psalm-suppress UnusedClass
+ * @phpcsSuppress NunoMaduro.PhpInsights.Domain.Insights.CyclomaticComplexityIsHigh
+ */
+    final class FWPageHelper
+    {
+/**
+ * make a new FWPage object
+ */
+        private static function doObject(Context $context, \RedBeanPHP\OODBBean $page) : void
+        {
+            if (!\preg_match('/\\\\/', $page->source))
+            { // no namespace so put it in \Pages
+                $page->source = '\\Pages\\'.$page->source;
+                \R::store($page);
+            }
+            $tl = \strtolower($page->source);
+            $tspl = \explode('\\', $page->source);
+            $base = \array_pop($tspl);
+            $lbase = \strtolower($base);
+            $namespace = \implode('\\', \array_filter($tspl));
+            $src = \preg_replace('/\\\\/', \DIRECTORY_SEPARATOR, $tl).'.php';
+            $file = $context->local()->makebasepath('class', $src);
+            if (!\file_exists($file))
+            { // make the file
+                $fd = \fopen($file, 'w');
+                if ($fd === FALSE)
+                {
+                    throw new \Framework\Exception\InternalError('Cannot create PHP file');
+                }
+                $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
+                \fwrite($fd, $context->local()->getRender('@util/pagesample.twig', ['pagename' => $page->name, 'namespace' => $namespace]));
+                \fclose($fd);
+            }
+/**
+ * @todo Make the render and template initialisation values a config value somewhere to detwig the system
+ */
+            $context->local()->initRender(['twig', ['templateDir' => 'twigs'/*, 'cache' => 'twigcache'*/]]);
+            $context->local()->makeTemplate($context, ['content', $lbase.'.twig']); // make a basic twig if there is not one there already.
+        }
+/**
+ * Handle a template page
+ *
+ * @todo detwig this
+ */
+        public static function doTemplate(Context $context, \RedBeanPHP\OODBBean $page) : void
+        {
+            if (!\preg_match('/\.twig$/', $page->source))
+            { // doesn't end in .twig
+                $page->source .= '.twig';
+            }
+            else
+            { // sometimes there are extra .twig extensions...
+                $page->source = \preg_replace('/(\.twig)+$/', '.twig', $page->source); // this removes extra .twigs .....
+            }
+            if (!\preg_match('/^@/', $page->source))
+            {
+                if (\preg_match('#/#', $page->source))
+                { // has directory separator characters in it so leave it alone - may be new top-level twig directory.
+                    $name = $page->source;
+                }
+                else
+                { // no namespace so put it in @content
+                    $page->source = '@content/'.$page->source;
+                    $name = ['content', $page->source];
+                    \R::store($page);
+                }
+            }
+            elseif (\preg_match('%^@content/(.*)%', $page->source, $m))
+            { // this is in the User twig content directory
+                $name = ['content', $m[1]];
+            }
+            elseif (\preg_match('%@([a-z]+)/(.*)%', $page->source, $m))
+            { // this is using a system twig
+                $name = ['framework', $m[1], $m[2]];
+            }
+            else
+            {
+                throw new \Framework\Exception\BadValue('Not recognised');
+            }
+            $context->local->makeTemplate($context, $name);
+        }
+    }
+?>
