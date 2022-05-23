22dc26ce5e48f78a60667a42df922788a4d4f5d0
rivets <lindsay.marshall@ncl.ac.uk>
2022-05-23 18:10:18 +0100

    Update beanlogops.php

--- class/framework/ajax/support/beanlogops.php
+++ class/framework/ajax/support/beanlogops.php
@@ -10,13 +10,14 @@
 
 /**
  * Enumeration for beanlog constants
+ * @phpcsSuppress NunoMaduro.PhpInsights.Domain.Insights.ForbiddenDefineFunctions
  */
     enum BeanLogOps : int
     {
         case CREATE = 0;
         case UPDATE = 1;
         case DELETE = 2;
-/** @phpcsSuppress NunoMaduro.PhpInsights.Domain.Insights.ForbiddenDefineFunctions **/
+
         public function label() : string
         {
             return match($this) {
