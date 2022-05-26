c8c952da38034068bb586c78d27e6d60172713bb
rivets <lindsay.marshall@ncl.ac.uk>
2022-05-26 10:49:32 +0100

    Update messagetype.php

--- class/framework/support/messagetype.php
+++ class/framework/support/messagetype.php
@@ -6,7 +6,7 @@
  * @copyright 2022 Newcastle University
  * @package Framework\Framework\Ajax\Support
  */
-    namespace Framework\Ajax\Support;
+    namespace Framework\Support;
 
 /**
  * Enumeration for beanlog constants
