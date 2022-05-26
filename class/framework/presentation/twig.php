87fcfc6b3ddeb3f9d9d77a89265aee6492ea9a5a
rivets <lindsay.marshall@ncl.ac.uk>
2022-05-26 11:47:07 +0100

    Update twig.php

--- class/framework/presentation/twig.php
+++ class/framework/presentation/twig.php
@@ -158,14 +158,15 @@
  */
         public function message(int|Msg $kind, array|string $value) : void
         {
-            if (\is_array($value))
-            {
-                $this->messages[$kind] = \array_merge($this->messages[$kind], $value);
-            }
-            else
-            {
-                $this->messages[$kind][] = $value;
-            }
+            $this->messages[$kind] = (\is_array($value) ? '\\array_merge' : '\\array_push')($this->messages[$kind], $value);
+            //if (\is_array($value))
+            //{
+            //    $this->messages[$kind] = \array_merge($this->messages[$kind], $value);
+            //}
+            //else
+            //{
+            //    $this->messages[$kind][] = $value;
+            //}
         }
 /**
  * Clear out messages
