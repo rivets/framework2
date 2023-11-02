dfa4daf351d5420283ae8050c396f8ef80ece863
rivets <l.f.marshall@me.com>
2023-11-02 15:05:34 +0000

    Update ajax.php
    Comment

--- ajax.php
+++ ajax.php
@@ -4,8 +4,7 @@
  *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
  * @copyright 2012-2020 Newcastle University
- */
-/**
+ *
  * The real work is all done in the Ajax class.
  */
     include 'class/config/framework.php';
